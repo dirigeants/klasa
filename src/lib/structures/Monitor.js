089e32ae8138b8f35be2df07d2dc88d348ecf181
bdistin <bdistin@gmail.com>
2017-07-17 16:10:51 -0500

    monitor docs

--- src/lib/structures/Monitor.js
+++ src/lib/structures/Monitor.js
@@ -5,6 +5,20 @@
  */
 class Monitor {
 
+	/**
+	 * @typedef {Object} MonitorOptions
+	 * @property {boolean} [enabled=true] Whether the monitor is enabled
+	 * @property {boolean} [ignoreBots=true] Whether the monitor ignores bots
+	 * @property {boolean} [ignoreSelf=true] Whether the monitor ignores itself
+	 */
+
+	/**
+	 * @param {KlasaClient} client The Klasa client
+	 * @param {string} dir The path to the core or user monitor pieces folder
+	 * @param {string} file The path from the pieces folder to the monitor file
+	 * @param {string} name The name of the monitor
+	 * @param {MonitorOptions} [options = {}] Optional Monitor settings
+	 */
 	constructor(client, dir, file, name, options = {}) {
 		this.client = client;
 		this.dir = dir;
@@ -16,31 +30,58 @@ class Monitor {
 		this.ignoreSelf = options.ignoreSelf || true;
 	}
 
+	/**
+	 * Reloads this monitor
+	 * @returns {Promise<Monitor>} The newly loaded monitor
+	 */
 	async reload() {
 		const mon = this.client.monitors.load(this.dir, this.file);
 		await mon.init();
 		return mon;
 	}
 
+	/**
+	 * Unloads this monitor
+	 * @returns {void}
+	 */
 	unload() {
 		return this.client.monitors.delete(this);
 	}
 
+	/**
+	 * Disables this monitor
+	 * @returns {Monitor} This monitor
+	 */
 	disable() {
 		this.enabled = false;
 		return this;
 	}
 
+	/**
+	 * Enables this monitor
+	 * @returns {Monitor} This monitor
+	 */
 	enable() {
 		this.enabled = true;
 		return this;
 	}
 
+	/**
+	 * The run method to be overwritten in actual event handlers
+	 * @param {external:Message} msg The discord message
+	 * @abstract
+	 * @returns {void}
+	 */
 	run() {
 		// Defined in extension Classes
 	}
 
-	init() {
+	/**
+	 * The init method to be optionaly overwritten in actual events
+	 * @abstract
+	 * @returns {Promise<void>}
+	 */
+	async init() {
 		// Optionally defined in extension Classes
 	}
 
