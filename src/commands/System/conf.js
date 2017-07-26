03b97ff2359166b96d95ad8a02a4d10ac6b289a8
bdistin <bdistin@gmail.com>
2017-07-25 20:45:55 -0500

    deduplicate conf

--- src/commands/System/conf.js
+++ src/commands/System/conf.js
@@ -15,15 +15,15 @@ module.exports = class extends Command {
 
 	async run(msg, [action, key, ...value]) {
 		const configs = msg.guild.settings;
+		if (action !== 'list' && !key) return msg.sendMessage('You must provide a key');
+		if (['set', 'remove'].includes(action) && !value[0]) return msg.sendMessage('You must provide a value');
+		if (['set', 'remove', 'reset'].includes(action) && !configs.id) await this.client.settingGateway.create(msg.guild);
 		this[action](msg, configs, key, value);
 
 		return null;
 	}
 
 	async set(msg, configs, key, value) {
-		if (!key) return msg.sendMessage('You must provide a key');
-		if (!value[0]) return msg.sendMessage('You must provide a value');
-		if (!configs.id) await this.client.settingGateway.create(msg.guild);
 		if (this.client.settingGateway.schemaManager.schema[key].array) {
 			await this.client.settingGateway.updateArray(msg.guild, 'add', key, value.join(' '));
 			return msg.sendMessage(`Successfully added the value \`${value.join(' ')}\` to the key: **${key}**`);
@@ -33,9 +33,6 @@ module.exports = class extends Command {
 	}
 
 	async remove(msg, configs, key, value) {
-		if (!key) return msg.sendMessage('You must provide a key');
-		if (!value[0]) return msg.sendMessage('You must provide a value');
-		if (!configs.id) await this.client.settingGateway.create(msg.guild);
 		if (!this.client.settingGateway.schema[key].array) return msg.sendMessage("This key is not array type. Use the action 'reset' instead.");
 		return this.client.settingGateway.updateArray(msg.guild, 'remove', key, value.join(' '))
 			.then(() => msg.sendMessage(`Successfully removed the value \`${value.join(' ')}\` from the key: **${key}**`))
@@ -43,14 +40,11 @@ module.exports = class extends Command {
 	}
 
 	async get(msg, configs, key) {
-		if (!key) return msg.sendMessage('You must provide a key');
 		if (!(key in configs)) return msg.sendMessage(`The key **${key}** does not seem to exist.`);
 		return msg.sendMessage(`The value for the key **${key}** is: \`${inspect(configs[key])}\``);
 	}
 
 	async reset(msg, configs, key) {
-		if (!key) return msg.sendMessage('You must provide a key');
-		if (!configs.id) await this.client.settingGateway.create(msg.guild);
 		const response = await this.client.settingGateway.reset(msg.guild, key);
 		return msg.sendMessage(`The key **${key}** has been reset to: \`${response}\``);
 	}
