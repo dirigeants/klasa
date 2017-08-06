87acb0b4acad6035a6d6660735f3337a7f7f9150
bdistin <bdistin@gmail.com>
2017-08-06 09:28:29 -0500

    fix usableCommands

--- src/extendables/usableCommands.js
+++ src/extendables/usableCommands.js
@@ -6,9 +6,9 @@ module.exports = class extends Extendable {
 		super(...args, ['Message']);
 	}
 
-	get extend() {
-		this.client.commands.filter(command => !this.client.commandInhibitors.some((inhibitor) => {
-			if (inhibitor.enabled && !inhibitor.spamProtection) return inhibitor.run(this.client, this, command);
+	async extend() {
+		return this.client.commands.filter(async command => await !this.client.commandInhibitors.some(async inhibitor => {
+			if (inhibitor.enabled && !inhibitor.spamProtection) return await inhibitor.run(this.client, this, command).catch(() => true);
 			return false;
 		}));
 	}
