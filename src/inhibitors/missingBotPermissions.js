f21dc487ee22315de0b52a8b9faece9f5a066e03
bdistin <bdistin@gmail.com>
2017-09-03 15:31:03 -0500

    cache the friendlier version of perms ahead of time

--- src/inhibitors/missingBotPermissions.js
+++ src/inhibitors/missingBotPermissions.js
@@ -16,11 +16,14 @@ module.exports = class extends Inhibitor {
 			'USE_EXTERNAL_EMOJIS',
 			'ADD_REACTIONS'
 		]);
+		this.friendlyPerms = Object.keys(Permissions.FLAGS).reduce((obj, key) => {
+			obj[key] = util.toTitleCase(key.split('_').join(' '));
+		}, {});
 	}
 
 	async run(msg, cmd) {
 		const missing = msg.channel.type === 'text' ? msg.channel.permissionsFor(this.client.user).missing(cmd.botPerms) : this.impliedPermissions.missing(cmd.botPerms);
-		if (missing.length > 0) throw msg.language.get('INHIBITOR_MISSING_BOT_PERMS', util.toTitleCase(missing.join(', ').split('_').join(' ')));
+		if (missing.length > 0) throw msg.language.get('INHIBITOR_MISSING_BOT_PERMS', missing.map(key => this.friendlyPerms[key]).join(', '));
 		return;
 	}
 
