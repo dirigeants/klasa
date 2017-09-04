4428bf0ac312b8d95b4d94fb13fd5c261635d541
bdistin <bdistin@gmail.com>
2017-09-04 07:39:14 -0500

    update extendables to add missing dm support and fix d.js breaking embed editing

--- src/extendables/sendMessage.js
+++ src/extendables/sendMessage.js
@@ -3,13 +3,13 @@ const { Extendable } = require('klasa');
 module.exports = class extends Extendable {
 
 	constructor(...args) {
-		super(...args, ['Message', 'TextChannel', 'DMChannel', 'GroupDMChannel']);
+		super(...args, ['Message', 'TextChannel', 'DMChannel', 'GroupDMChannel', 'User']);
 	}
 
-	extend(content, options) {
+	extend(content = '', options = {}) {
 		if (!this.channel) return this.send(content, options);
 		const commandMessage = this.client.commandMessages.get(this.id);
-		if (commandMessage && (!options || !('files' in options))) return commandMessage.response.edit(content, options);
+		if (commandMessage && (!options || !('files' in options))) return commandMessage.response.edit(content, { embed: options.embed || null });
 		return this.channel.send(content, options)
 			.then((mes) => {
 				if (mes.constructor.name === 'Message' && (!options || !('files' in options))) this.client.commandMessages.set(this.id, { trigger: this, response: mes });
