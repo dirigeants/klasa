9372b6f567c5d7d97f5f610d49d4fdc293bc4ec8
MrJacz <jacknagle22@gmail.com>
2017-09-07 10:41:54 +1000

    Remind me to fix indent

--- src/commands/System/ping.js
+++ src/commands/System/ping.js
@@ -3,7 +3,7 @@ const { Command } = require('klasa');
 module.exports = class extends Command {
 
 	constructor(...args) {
-		super(...args, { description: 'Ping/Pong command. I wonder what this does? /sarcasm' });
+		super(...args, { description: 'Ping/Pong command. I wonder what this does? /sarcasm', botPerms: ["SEND_MESSAGES"] });
 	}
 
 	async run(msg) {
