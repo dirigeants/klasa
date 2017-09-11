e589be3c45ef295669d8e92c53b18355ba439c0b
bdistin <bdistin@gmail.com>
2017-09-10 20:22:54 -0500

    drop moment duration format

--- src/commands/System/stats.js
+++ src/commands/System/stats.js
@@ -1,7 +1,6 @@
 const { Command, version: klasaVersion } = require('klasa');
 const { version: discordVersion } = require('discord.js');
 const moment = require('moment');
-require('moment-duration-format');
 
 module.exports = class extends Command {
 
@@ -15,7 +14,7 @@ module.exports = class extends Command {
 			'= STATISTICS =',
 			'',
 			`• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
-			`• Uptime     :: ${duration}`,
+			`• Uptime     :: ${moment(Date.now() - (process.uptime() * 1000)).toNow(true)}`,
 			`• Users      :: ${this.client.users.size.toLocaleString()}`,
 			`• Servers    :: ${this.client.guilds.size.toLocaleString()}`,
 			`• Channels   :: ${this.client.channels.size.toLocaleString()}`,
