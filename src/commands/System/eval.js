aef58905408c5ba50c2bd9543615ba806d83c7fd
Hutch Moore <tech_hutch@verizon.net>
2017-10-19 20:24:38 -0400

    Eval
    * Remove JSDocs from methods in eval cmd
    * Moved a bunch of methods & TimeoutError to util
    * Changed handleEval to return an object, for clarity (not that it really matters in this context, I suppose)
    * Added a note to the extended help

--- src/commands/System/eval.js
+++ src/commands/System/eval.js
@@ -1,6 +1,6 @@
 const now = require('performance-now');
 const { MessageAttachment } = require('discord.js');
-const { Command, util: { sleep } } = require('klasa');
+const { Command, util } = require('klasa');
 const { inspect } = require('util');
 
 module.exports = class extends Command {
@@ -10,20 +10,21 @@ module.exports = class extends Command {
 			aliases: ['ev'],
 			permLevel: 10,
 			description: 'Evaluates arbitrary JavaScript. Reserved for bot owner.',
-			usage: '<expression:str>'
+			usage: '<expression:str>',
+			extendedHelp: `See the Klasa Pieces repo if you'd like an eval with more features.`
 		});
 
 		// The depth to inspect the evaled output to, if it's not a string
 		this.inspectionDepth = 0;
-		// this.getTypeStr shouldn't recurse more than once, but just in case
-		this.typeRecursionLimit = 2;
 		// How long to wait for promises to resolve
-		this.timeout = 10000;
+		this.wait = 10000;
+		// The depth to get the types of nested data structures, recursively
+		this.typeDepth = 2;
 	}
 
 	async run(msg, [code]) {
 		try {
-			const [evaled, topLine] = await this.handleEval(code, msg);
+			const { evaled, topLine } = await this.handleEval(code, msg);
 
 			if (this.isTooLong(evaled, topLine)) {
 				// Upload as a file attachment and send to channel
@@ -37,30 +38,24 @@ module.exports = class extends Command {
 		}
 	}
 
-	/**
-	 * Eval the code and get info on the type of the result
-	 * @param {string} code The code obvs
-	 * @param {DiscordMessage} msg The message, so it's available to the eval
-	 * @returns {Array<string>}
-	 */
 	async handleEval(code, msg) { // eslint-disable-line no-unused-vars
 		const start = now();
 		const evaledOriginal = eval(code); // eslint-disable-line no-eval
 		const syncEnd = now();
-		const evaledTimeout = this.timeoutPromise(evaledOriginal, this.timeout);
+		const evaledTimeout = util.timeoutPromise(evaledOriginal, this.wait);
 		// Awaiting a non-promise returns the non-promise
 		let evaledValue = await evaledTimeout;
 		const asyncEnd = now();
 
-		const evaledIsThenable = this.isThenable(evaledOriginal);
+		const evaledIsThenable = util.isThenable(evaledOriginal);
 
 		// We're doing this checking here so it's not counted in the performance-now timeing
 		// And if the promise timed out, just show the promise
-		if (!evaledIsThenable || evaledValue instanceof TimeoutError) evaledValue = evaledOriginal;
+		if (!evaledIsThenable || evaledValue instanceof util.TimeoutError) evaledValue = evaledOriginal;
 
 		const time = evaledIsThenable ?
-			`⏱${this.getNiceDuration(syncEnd - start)}<${this.getNiceDuration(asyncEnd - syncEnd)}>` :
-			`⏱${this.getNiceDuration(syncEnd - start)}`;
+			`⏱${util.getNiceDuration(syncEnd - start)}<${util.getNiceDuration(asyncEnd - syncEnd)}>` :
+			`⏱${util.getNiceDuration(syncEnd - start)}`;
 
 		const topLine = `${await this.getTypeStr(
 			evaledOriginal,
@@ -69,207 +64,32 @@ module.exports = class extends Command {
 
 		if (typeof evaledValue !== 'string') evaledValue = inspect(evaledValue, { depth: this.inspectionDepth });
 
-		return [evaledValue, topLine];
+		return { evaled: evaledValue, topLine };
 	}
 
-	/**
-	 * Checks if the output will be more than 2,000 characters
-	 * @param {string} evaled The evaled output (as a string)
-	 * @param {string} topLine The line with the type and time info
-	 * @returns {boolean}
-	 */
 	isTooLong(evaled, topLine) {
 		// 1988 is 2000 - 12 (the chars that are added, "`...`\n```js\n...```")
 		return evaled.length > 1988 - topLine.length;
 	}
 
-	/**
-	 * Get the type string of the evaled result
-	 * @param {*} value The value to get the type string for
-	 * @param {?Promise} [awaitedPromise] The promise that was already `await`ed earlier; this also acts
-	 *  as a surrogate, so that if the original promise was wrapped in a timeout promise, the original
-	 *  promise can be examined, while the already-awaited surrogate is awaited
-	 * @param {number} [i=0] Just an iteration count to prevent infinite loops
-	 * @returns {string}
-	 */
 	async getTypeStr(value, awaitedPromise = null, i = 0) {
-		if (value instanceof TimeoutError) return '?';
+		if (value instanceof util.TimeoutError) return '?';
 
-		const { basicType, type } = this.getComplexType(value);
+		const { basicType, type } = util.getComplexType(value);
 		if (basicType === 'object') {
-			if (this.isThenable(value)) {
-				return i <= this.typeRecursionLimit ?
+			if (util.isThenable(value)) {
+				return i <= this.typeDepth ?
 				// But we're gonna await the already-awaited promise, for efficiency
 					`${type}<${await this.getTypeStr(await awaitedPromise, null, i + 1)}>` :
 					`${type}<?>`;
 			}
-			if (Array.isArray(value)) return `${type}${this.getArrayType(value)}`;
-			if (value instanceof Map) return `${type}${this.getMapType(value)}`;
-			if (value instanceof Set) return `${type}${this.getSetType(value)}`;
-			return `${type}${this.getObjectType(value)}`;
+			if (Array.isArray(value)) return `${type}${util.getArrayType(value, this.typeDepth)}`;
+			if (value instanceof Map) return `${type}${util.getMapType(value, this.typeDepth)}`;
+			if (value instanceof Set) return `${type}${util.getSetType(value, this.typeDepth)}`;
+			return `${type}${util.getObjectType(value, this.typeDepth)}`;
 		}
-		if (basicType === 'function') return `${type}${this.getFunctionType(value)}`;
+		if (basicType === 'function') return `${type}${util.getFunctionType(value, this.typeDepth)}`;
 		return type;
 	}
 
-	/**
-	 * Get the type of value
-	 * 
-	 * A better version of the `typeof` operator, basically.
-	 * @param {*} value The object or primitive whose type is to be returned
-	 * @returns {string}
-	 */
-	getType(value) {
-		if (value == null) return String(value); // eslint-disable-line eqeqeq
-		return typeof value;
-	}
-	/**
-	 * Get the class (constructor) name of value
-	 * @param {*} value The object whose class name is to be returned
-	 * @returns {string}
-	 */
-	getClass(value) {
-		return value && value.constructor && value.constructor.name ?
-			value.constructor.name :
-			{}.toString.call(value).match(/\[object (\w+)\]/)[1];
-	}
-	/**
-	 * Get the type info for value
-	 * @param {*} value The object or primitive whose complex type is to be returned
-	 * @returns {{basicType: string, type: string}}
-	 */
-	getComplexType(value) {
-		const basicType = this.getType(value);
-		if (basicType === 'object' || basicType === 'function') return { basicType, type: this.getClass(value) };
-		return { basicType, type: basicType };
-	}
-
-	/**
-	 * Get the arity of fn
-	 * @param {Function} fn The function whose arity is to be returned
-	 * @returns {string}
-	 */
-	getFunctionType(fn) {
-		return `(${fn.length}-arity)`;
-	}
-	/**
-	 * Get the type of array's elements
-	 * @param {Array} array The array whose element type is to be returned
-	 * @param {number} [i=0] Just an iteration count to prevent infinite loops
-	 * @returns {string}
-	 */
-	getArrayType(array, i = 0) {
-		return `<${this._getObjType(array, i)}>`;
-	}
-	/**
-	 * Get the type of obj's elements
-	 * @param {Object} obj The object whose element type is to be returned
-	 * @param {number} [i=0] Just an iteration count to prevent infinite loops
-	 * @returns {string}
-	 */
-	getObjectType(obj, i = 0) {
-		const type = this._getObjType(Object.values(obj), i);
-		return type.length > 0 ? `<${this.getComplexType('').type}, ${type}>` : '<>';
-	}
-	/**
-	 * Get the type of map's values
-	 * @param {Map} map The map whose value type is to be returned
-	 * @param {number} [i=0] Just an iteration count to prevent infinite loops
-	 * @returns {string}
-	 */
-	getMapType(map, i = 0) {
-		const keyType = this._getObjType(Array.from(map.keys()), i);
-		const valueType = this._getObjType(Array.from(map.values()), i);
-		return valueType.length > 0 ? `<${keyType}, ${valueType}>` : '<>';
-	}
-	/**
-	 * Get the type of set's values
-	 * @param {Set} set The set whose value type is to be returned
-	 * @param {number} [i=0] Just an iteration count to prevent infinite loops
-	 * @returns {string}
-	 */
-	getSetType(set, i = 0) {
-		return `<${this._getObjType(Array.from(set.values()), i)}>`;
-	}
-	/**
-	 * Get the type of values's elements
-	 * @param {Array} values The array whose element type is to be returned
-	 * @param {number} i Just an iteration count to prevent infinite loops
-	 * @returns {string}
-	 */
-	_getObjType(values, i) {
-		if (!Array.isArray(values)) throw new TypeError("You're using this function wrong; `values` must be an array");
-		if (typeof i !== 'number') throw new TypeError('`i` is missing');
-		// Collections have useful methods, which work on Sets.
-		const Coll = this.client.methods.Collection.prototype;
-
-		const objTypes = new Set(values.map(val => this.getComplexType(val).type));
-		const nonNullTypes = new Set();
-		const nullTypes = new Set();
-		for (const type of objTypes.values()) {
-			if (['null', 'undefined'].includes(type)) nullTypes.add(type);
-			else nonNullTypes.add(type);
-		}
-
-		if (nonNullTypes.size > 1) return '*';
-		if (nonNullTypes.size === 1) {
-			const type = Coll.first.call(nonNullTypes);
-			const value = values.find(val => val != null); // eslint-disable-line eqeqeq
-			const nestedType = this.getComplexType(value);
-			let nestedTypeStr = '';
-			if (i < this.typeRecursionLimit) {
-				if (nestedType.basicType === 'object') {
-					if (Array.isArray(value)) nestedTypeStr = this.getArrayType(value, i + 1);
-					else if (value instanceof Map) nestedTypeStr = this.getMapType(value, i + 1);
-					else if (value instanceof Set) nestedTypeStr = this.getSetType(value, i + 1);
-					else nestedTypeStr = this.getObjectType(value, i + 1);
-				} else if (nestedType.basicType === 'function') { nestedTypeStr = this.getFunctionType(value); }
-			}
-			if (nullTypes.size > 0) return `?${type}${nestedTypeStr}`;
-			return `${type}${nestedTypeStr}`;
-		}
-
-		// No types besides, possibly, "null" and "undefined"
-		if (nullTypes.size > 1) return 'null|undefined';
-		if (nullTypes.size === 1) return Coll.first.call(nullTypes);
-
-		// No types at all, i.e. no elements at all
-		return '';
-	}
-
-	/**
-	 * Present time duration in a nice way
-	 * @param {number} time A duration in milliseconds
-	 * @returns {string}
-	 */
-	getNiceDuration(time) {
-		if (time >= 1000) return `${(time / 1000).toFixed(2)}s`;
-		if (time >= 1) return `${time.toFixed(2)}ms`;
-		return `${(time * 1000).toFixed(2)}μs`;
-	}
-
-	/**
-	 * Determines whether the passed value is an Array
-	 * @param {*} value The value to be checked
-	 * @returns {boolean}
-	 */
-	isThenable(value) {
-		return value && typeof value.then === 'function';
-	}
-
-	/**
-	 * Wrap a promise in a promise that will timeout in a certain amount of time
-	 * 
-	 * Whichever promise (the inputted one or the timeout one) resolves first will have its value be
-	 * the resolved value of the returned promise.
-	 * @param {Promise} promise The promise to wrap
-	 * @param {number} timeout How long the new promise should wait before timing out
-	 * @returns {Promise}
-	 */
-	timeoutPromise(promise, timeout) {
-		return Promise.race([promise, sleep(timeout, new TimeoutError('Promise timed out'))]);
-	}
-
 };
-
-class TimeoutError extends Error {}
--- src/lib/util/util.js
+++ src/lib/util/util.js
@@ -1,5 +1,6 @@
 const { promisify } = require('util');
 const { exec } = require('child_process');
+const { Collection } = require('discord.js');
 const zws = String.fromCharCode(8203);
 let sensitivePattern;
 
@@ -98,6 +99,182 @@ class Util {
 		}
 	}
 
+	/**
+   * Get the type of value. A better version of the `typeof` operator, basically
+   * @param {*} value The object or primitive whose type is to be returned
+   * @returns {string}
+   */
+	static getType(value) {
+		if (value == null) return String(value); // eslint-disable-line eqeqeq
+		return typeof value;
+	}
+
+	/**
+   * Get the class (constructor) name of value
+   * @param {*} value The object whose class name is to be returned
+   * @returns {string}
+   */
+	static getClass(value) {
+		return value && value.constructor && value.constructor.name ?
+			value.constructor.name :
+			{}.toString.call(value).match(/\[object (\w+)\]/)[1];
+	}
+
+	/**
+   * Get the type info for value
+   * @param {*} value The object or primitive whose complex type is to be returned
+   * @returns {{basicType: string, type: string}}
+   */
+	static getComplexType(value) {
+		const basicType = this.getType(value);
+		if (basicType === 'object' || basicType === 'function') return { basicType, type: this.getClass(value) };
+		return { basicType, type: basicType };
+	}
+
+	/**
+   * Get the arity of fn
+   * @param {Function} fn The function whose arity is to be returned
+   * @returns {string}
+   */
+	static getFunctionType(fn) {
+		return `(${fn.length}-arity)`;
+	}
+
+	/**
+   * Get the type of array's elements
+   * @param {Array} array The array whose element type is to be returned
+   * @param {number} depth The depth to get the type, recursively
+   * @param {number} [i=0] Just an iteration count to prevent infinite loops
+   * @returns {string}
+   */
+	static getArrayType(array, depth, i = 0) {
+		if (typeof depth !== 'number') throw new TypeError('`depth` is missing, or not a number');
+		return `<${this._getObjType(array, depth, i)}>`;
+	}
+
+	/**
+   * Get the type of obj's elements
+   * @param {Object} obj The object whose element type is to be returned
+   * @param {number} depth The depth to get the type, recursively
+   * @param {number} [i=0] Just an iteration count to prevent infinite loops
+   * @returns {string}
+   */
+	static getObjectType(obj, depth, i = 0) {
+		if (typeof depth !== 'number') throw new TypeError('`depth` is missing, or not a number');
+		const type = this._getObjType(Object.values(obj), depth, i);
+		return type.length > 0 ? `<${this.getComplexType('').type}, ${type}>` : '<>';
+	}
+
+	/**
+   * Get the type of map's values
+   * @param {Map} map The map whose value type is to be returned
+   * @param {number} depth The depth to get the type, recursively
+   * @param {number} [i=0] Just an iteration count to prevent infinite loops
+   * @returns {string}
+   */
+	static getMapType(map, depth, i = 0) {
+		if (typeof depth !== 'number') throw new TypeError('`depth` is missing, or not a number');
+		const keyType = this._getObjType(Array.from(map.keys()), depth, i);
+		const valueType = this._getObjType(Array.from(map.values()), depth, i);
+		return valueType.length > 0 ? `<${keyType}, ${valueType}>` : '<>';
+	}
+
+	/**
+   * Get the type of set's values
+   * @param {Set} set The set whose value type is to be returned
+   * @param {number} depth The depth to get the type, recursively
+   * @param {number} [i=0] Just an iteration count to prevent infinite loops
+   * @returns {string}
+   */
+	static getSetType(set, depth, i = 0) {
+		if (typeof depth !== 'number') throw new TypeError('`depth` is missing, or not a number');
+		return `<${this._getObjType(Array.from(set.values()), depth, i)}>`;
+	}
+
+	/**
+   * Get the type of values's elements
+   * @private
+   * @param {Array} values The array whose element type is to be returned
+   * @param {number} depth The depth to get the type, recursively
+   * @param {number} i Just an iteration count to prevent infinite loops
+   * @returns {string}
+   */
+	static _getObjType(values, depth, i) {
+		if (!Array.isArray(values)) throw new TypeError("You're using this function wrong; `values` must be an array");
+		if (typeof depth !== 'number') throw new TypeError('`depth` is missing, or not a number');
+		if (typeof i !== 'number') throw new TypeError('`i` is missing, or not a number');
+		// Collections have useful methods, which work on Sets.
+		const Coll = Collection.prototype;
+
+		const objTypes = new Set(values.map(val => this.getComplexType(val).type));
+		const nonNullTypes = new Set();
+		const nullTypes = new Set();
+		for (const type of objTypes.values()) {
+			if (['null', 'undefined'].includes(type)) nullTypes.add(type);
+			else nonNullTypes.add(type);
+		}
+
+		if (nonNullTypes.size > 1) return '*';
+		if (nonNullTypes.size === 1) {
+			const type = Coll.first.call(nonNullTypes);
+			const value = values.find(val => val != null); // eslint-disable-line eqeqeq
+			const nestedType = this.getComplexType(value);
+			let nestedTypeStr = '';
+			if (i < depth) {
+				if (nestedType.basicType === 'object') {
+					if (Array.isArray(value)) nestedTypeStr = this.getArrayType(value, depth, i + 1);
+					else if (value instanceof Map) nestedTypeStr = this.getMapType(value, depth, i + 1);
+					else if (value instanceof Set) nestedTypeStr = this.getSetType(value, depth, i + 1);
+					else nestedTypeStr = this.getObjectType(value, depth, i + 1);
+				} else if (nestedType.basicType === 'function') {
+					nestedTypeStr = this.getFunctionType(value, depth, i + 1);
+				}
+			}
+			if (nullTypes.size > 0) return `?${type}${nestedTypeStr}`;
+			return `${type}${nestedTypeStr}`;
+		}
+
+		// No types besides, possibly, "null" and "undefined"
+		if (nullTypes.size > 1) return 'null|undefined';
+		if (nullTypes.size === 1) return Coll.first.call(nullTypes);
+
+		// No types at all, i.e. no elements at all
+		return '';
+	}
+
+	/**
+   * Determines whether the passed value is an Array.
+   * @param {*} value The value to be checked.
+   * @returns {boolean}
+   */
+	static isThenable(value) {
+		return value && typeof value.then === 'function';
+	}
+
+	/**
+   * Wrap a promise in a promise that will timeout in a certain amount of time.
+   * 
+   * Whichever promise (the inputted one or the timeout one) resolves first will have its value be
+   * the resolved value of the returned promise.
+   * @param {Promise} promise The promise to wrap.
+   * @param {number} timeout How long the new promise should wait before timing out.
+   * @returns {Promise}
+   */
+	static timeoutPromise(promise, timeout) {
+		return Promise.race([promise, this.sleep(timeout, new this.TimeoutError('Promise timed out'))]);
+	}
+
+	/**
+   * Present time duration in a nice way
+   * @param {number} time A duration in milliseconds
+   * @returns {string}
+   */
+	static getNiceDuration(time) {
+		if (time >= 1000) return `${(time / 1000).toFixed(2)}s`;
+		if (time >= 1) return `${time.toFixed(2)}ms`;
+		return `${(time * 1000).toFixed(2)}μs`;
+	}
+
 }
 
 /**
@@ -131,5 +308,10 @@ Util.exec = promisify(exec);
  * @returns {Promise<any>} The args value passed in
  */
 Util.sleep = promisify(setTimeout);
+
+/**
+ * Used to mark when a promise has timed out
+ */
+Util.TimeoutError = class TimeoutError extends Error {};
 
 module.exports = Util;
