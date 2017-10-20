edfaefc9e9ca5735a6aea4ab683d7743c0beb032
Hutch Moore <tech_hutch@verizon.net>
2017-10-20 08:11:35 -0400

    Fixed util docblock indentation

--- src/lib/util/util.js
+++ src/lib/util/util.js
@@ -100,20 +100,20 @@ class Util {
 	}
 
 	/**
-   * Get the type of value. A better version of the `typeof` operator, basically
-   * @param {*} value The object or primitive whose type is to be returned
-   * @returns {string}
-   */
+	 * Get the type of value. A better version of the `typeof` operator, basically
+	 * @param {*} value The object or primitive whose type is to be returned
+	 * @returns {string}
+	 */
 	static getType(value) {
 		if (value == null) return String(value); // eslint-disable-line eqeqeq
 		return typeof value;
 	}
 
 	/**
-   * Get the class (constructor) name of value
-   * @param {*} value The object whose class name is to be returned
-   * @returns {string}
-   */
+	 * Get the class (constructor) name of value
+	 * @param {*} value The object whose class name is to be returned
+	 * @returns {string}
+	 */
 	static getClass(value) {
 		return value && value.constructor && value.constructor.name ?
 			value.constructor.name :
@@ -121,10 +121,10 @@ class Util {
 	}
 
 	/**
-   * Get the type info for value
-   * @param {*} value The object or primitive whose complex type is to be returned
-   * @returns {{basicType: string, type: string}}
-   */
+	 * Get the type info for value
+	 * @param {*} value The object or primitive whose complex type is to be returned
+	 * @returns {{basicType: string, type: string}}
+	 */
 	static getComplexType(value) {
 		const basicType = this.getType(value);
 		if (basicType === 'object' || basicType === 'function') return { basicType, type: this.getClass(value) };
@@ -132,33 +132,33 @@ class Util {
 	}
 
 	/**
-   * Get the arity of fn
-   * @param {Function} fn The function whose arity is to be returned
-   * @returns {string}
-   */
+	 * Get the arity of fn
+	 * @param {Function} fn The function whose arity is to be returned
+	 * @returns {string}
+	 */
 	static getFunctionType(fn) {
 		return `(${fn.length}-arity)`;
 	}
 
 	/**
-   * Get the type of array's elements
-   * @param {Array} array The array whose element type is to be returned
-   * @param {number} depth The depth to get the type, recursively
-   * @param {number} [i=0] Just an iteration count to prevent infinite loops
-   * @returns {string}
-   */
+	 * Get the type of array's elements
+	 * @param {Array} array The array whose element type is to be returned
+	 * @param {number} depth The depth to get the type, recursively
+	 * @param {number} [i=0] Just an iteration count to prevent infinite loops
+	 * @returns {string}
+	 */
 	static getArrayType(array, depth, i = 0) {
 		if (typeof depth !== 'number') throw new TypeError('`depth` is missing, or not a number');
 		return `<${this._getObjType(array, depth, i)}>`;
 	}
 
 	/**
-   * Get the type of obj's elements
-   * @param {Object} obj The object whose element type is to be returned
-   * @param {number} depth The depth to get the type, recursively
-   * @param {number} [i=0] Just an iteration count to prevent infinite loops
-   * @returns {string}
-   */
+	 * Get the type of obj's elements
+	 * @param {Object} obj The object whose element type is to be returned
+	 * @param {number} depth The depth to get the type, recursively
+	 * @param {number} [i=0] Just an iteration count to prevent infinite loops
+	 * @returns {string}
+	 */
 	static getObjectType(obj, depth, i = 0) {
 		if (typeof depth !== 'number') throw new TypeError('`depth` is missing, or not a number');
 		const type = this._getObjType(Object.values(obj), depth, i);
@@ -166,12 +166,12 @@ class Util {
 	}
 
 	/**
-   * Get the type of map's values
-   * @param {Map} map The map whose value type is to be returned
-   * @param {number} depth The depth to get the type, recursively
-   * @param {number} [i=0] Just an iteration count to prevent infinite loops
-   * @returns {string}
-   */
+	 * Get the type of map's values
+	 * @param {Map} map The map whose value type is to be returned
+	 * @param {number} depth The depth to get the type, recursively
+	 * @param {number} [i=0] Just an iteration count to prevent infinite loops
+	 * @returns {string}
+	 */
 	static getMapType(map, depth, i = 0) {
 		if (typeof depth !== 'number') throw new TypeError('`depth` is missing, or not a number');
 		const keyType = this._getObjType(Array.from(map.keys()), depth, i);
@@ -180,25 +180,25 @@ class Util {
 	}
 
 	/**
-   * Get the type of set's values
-   * @param {Set} set The set whose value type is to be returned
-   * @param {number} depth The depth to get the type, recursively
-   * @param {number} [i=0] Just an iteration count to prevent infinite loops
-   * @returns {string}
-   */
+	 * Get the type of set's values
+	 * @param {Set} set The set whose value type is to be returned
+	 * @param {number} depth The depth to get the type, recursively
+	 * @param {number} [i=0] Just an iteration count to prevent infinite loops
+	 * @returns {string}
+	 */
 	static getSetType(set, depth, i = 0) {
 		if (typeof depth !== 'number') throw new TypeError('`depth` is missing, or not a number');
 		return `<${this._getObjType(Array.from(set.values()), depth, i)}>`;
 	}
 
 	/**
-   * Get the type of values's elements
-   * @private
-   * @param {Array} values The array whose element type is to be returned
-   * @param {number} depth The depth to get the type, recursively
-   * @param {number} i Just an iteration count to prevent infinite loops
-   * @returns {string}
-   */
+	 * Get the type of values's elements
+	 * @private
+	 * @param {Array} values The array whose element type is to be returned
+	 * @param {number} depth The depth to get the type, recursively
+	 * @param {number} i Just an iteration count to prevent infinite loops
+	 * @returns {string}
+	 */
 	static _getObjType(values, depth, i) {
 		if (!Array.isArray(values)) throw new TypeError("You're using this function wrong; `values` must be an array");
 		if (typeof depth !== 'number') throw new TypeError('`depth` is missing, or not a number');
@@ -243,32 +243,32 @@ class Util {
 	}
 
 	/**
-   * Determines whether the passed value is an Array.
-   * @param {*} value The value to be checked.
-   * @returns {boolean}
-   */
+	 * Determines whether the passed value is an Array.
+	 * @param {*} value The value to be checked.
+	 * @returns {boolean}
+	 */
 	static isThenable(value) {
 		return value && typeof value.then === 'function';
 	}
 
 	/**
-   * Wrap a promise in a promise that will timeout in a certain amount of time.
-   * 
-   * Whichever promise (the inputted one or the timeout one) resolves first will have its value be
-   * the resolved value of the returned promise.
-   * @param {Promise} promise The promise to wrap.
-   * @param {number} timeout How long the new promise should wait before timing out.
-   * @returns {Promise}
-   */
+	 * Wrap a promise in a promise that will timeout in a certain amount of time.
+	 * 
+	 * Whichever promise (the inputted one or the timeout one) resolves first will have its value be
+	 * the resolved value of the returned promise.
+	 * @param {Promise} promise The promise to wrap.
+	 * @param {number} timeout How long the new promise should wait before timing out.
+	 * @returns {Promise}
+	 */
 	static timeoutPromise(promise, timeout) {
 		return Promise.race([promise, this.sleep(timeout, new this.TimeoutError('Promise timed out'))]);
 	}
 
 	/**
-   * Present time duration in a nice way
-   * @param {number} time A duration in milliseconds
-   * @returns {string}
-   */
+	 * Present time duration in a nice way
+	 * @param {number} time A duration in milliseconds
+	 * @returns {string}
+	 */
 	static getNiceDuration(time) {
 		if (time >= 1000) return `${(time / 1000).toFixed(2)}s`;
 		if (time >= 1) return `${time.toFixed(2)}ms`;
