64c177d04406ad95520c5cadaa06950d92d5565d
kyraNET <kyradiscord@gmail.com>
2017-11-21 18:24:07 +0100

    Undo #88

--- src/lib/structures/Extendable.js
+++ src/lib/structures/Extendable.js
@@ -1,6 +1,5 @@
 const Piece = require('./interfaces/Piece');
 const Discord = require('discord.js');
-const util = require('util');
 
 /**
  * Base class for all Klasa Extendables. See {@tutorial CreatingExtendables} for more information how to use this class
@@ -16,7 +15,6 @@ class Extendable {
 	 * @property {string} [name = theFileName] The name of the extendable
 	 * @property {boolean} [enabled = true] If the extendable is enabled or not
 	 * @property {boolean} [klasa = false] If the extendable is for Klasa instead of Discord.js
-	 * @property {string} [deprecated = null] If the extendable should be deprecated or not
 	 */
 
 	/**
@@ -82,13 +80,6 @@ class Extendable {
 		 * @type {boolean}
 		 */
 		this.target = options.klasa ? require('klasa') : Discord;
-
-		/**
-		 * Whether the extendable should be deprecated or not
-     * @since 0.4.0
-		 * @type {?string}
-		 */
-		this.deprecated = typeof options.deprecated === 'string' ? options.deprecated : null;
 	}
 
 	/**
@@ -129,21 +120,14 @@ class Extendable {
 	 */
 	enable() {
 		this.enabled = true;
-		const descriptor = Object.getOwnPropertyDescriptor(this.constructor.prototype, 'extend');
-		if (this.deprecated !== null) {
-			if (descriptor.value) descriptor.value = util.deprecate(descriptor.value, this.deprecated);
-			if (descriptor.get) descriptor.get = util.deprecate(descriptor.get, this.deprecated);
-			if (descriptor.set) descriptor.set = util.deprecate(descriptor.set, this.deprecated);
-		}
-
-		for (const structure of this.appliesTo) Object.defineProperty(this.target[structure].prototype, this.name, descriptor);
+		for (const structure of this.appliesTo) Object.defineProperty(this.target[structure].prototype, this.name, Object.getOwnPropertyDescriptor(this.constructor.prototype, 'extend'));
 		return this;
 	}
 
 	// left for documentation
 	/* eslint-disable no-empty-function */
-	async reload() {}
-	unload() {}
+	async reload() { }
+	unload() { }
 	/* eslint-enable no-empty-function */
 
 }
