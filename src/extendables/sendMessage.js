5280d713875243f6346a93cb1ea8be3602b0a25f
AoDude <bdistin@gmail.com>
2017-11-27 12:01:51 -0600

    Bug Fixes and cleanup

--- src/extendables/sendMessage.js
+++ src/extendables/sendMessage.js
@@ -3,25 +3,11 @@ const { Extendable } = require('klasa');
 module.exports = class extends Extendable {
 
 	constructor(...args) {
-		super(...args, ['Message', 'TextChannel', 'DMChannel', 'GroupDMChannel', 'User']);
+		super(...args, ['TextChannel', 'DMChannel', 'GroupDMChannel', 'User']);
 	}
 
 	extend(content, options) {
-		if (!options && typeof content === 'object' && !Array.isArray(content)) {
-			options = content;
-			content = '';
-		} else if (!options) {
-			options = {};
-		}
-		options.embed = options.embed || null;
-		if (!this.channel) return this.send(content, options);
-		const commandMessage = this.client.commandMessages.get(this.id);
-		if (commandMessage && (!options || !('files' in options))) return commandMessage.response.edit(content, options);
-		return this.channel.send(content, options)
-			.then((mes) => {
-				if (mes.constructor.name === 'Message' && (!options || !('files' in options))) this.client.commandMessages.set(this.id, { trigger: this, response: mes });
-				return mes;
-			});
+		return this.send(content, options);
 	}
 
 };
--- src/inhibitors/requiredSettings.js
+++ src/inhibitors/requiredSettings.js
@@ -4,7 +4,7 @@ module.exports = class extends Inhibitor {
 
 	async run(msg, cmd) {
 		if (cmd.requiredSettings.length === 0) return;
-		const settings = cmd.requiredSettings.filter(setting => !msg.guildSettings[setting]);
+		const settings = cmd.requiredSettings.filter(setting => !msg.guildConfigs[setting]);
 		if (settings.length > 0) throw msg.language.get('INHIBITOR_REQUIRED_SETTINGS', settings);
 		return;
 	}
