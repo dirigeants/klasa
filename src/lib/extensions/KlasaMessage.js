c15bd37538252d2f59e7ed94f1b63f90d94fb430
AoDude <bdistin@gmail.com>
2017-11-27 11:51:54 -0600

    move validate args from CommandMessage to KlasaMessage (oops)

--- src/lib/extensions/KlasaMessage.js
+++ src/lib/extensions/KlasaMessage.js
@@ -182,6 +182,98 @@ module.exports = Structures.extend('Message', Message => {
 		}
 
 		/**
+		 * Validates and resolves args into parameters
+		 * @since 0.0.1
+		 * @private
+		 * @returns {any[]} The resolved parameters
+		 */
+		async validateArgs() {
+			if (this.params.length >= this.cmd.usage.parsedUsage.length && this.params.length >= this.args.length) {
+				return this.params;
+			} else if (this.cmd.usage.parsedUsage[this.params.length]) {
+				if (this.cmd.usage.parsedUsage[this.params.length].type !== 'repeat') {
+					this._currentUsage = this.cmd.usage.parsedUsage[this.params.length];
+				} else if (this.cmd.usage.parsedUsage[this.params.length].type === 'repeat') {
+					this._currentUsage.type = 'optional';
+					this._repeat = true;
+				}
+			} else if (!this._repeat) {
+				return this.params;
+			}
+			if (this._currentUsage.type === 'optional' && (this.args[this.params.length] === undefined || this.args[this.params.length] === '')) {
+				if (this.cmd.usage.parsedUsage.slice(this.params.length).some(usage => usage.type === 'required')) {
+					this.args.splice(this.params.length, 0, undefined);
+					this.args.splice(this.params.length, 1, null);
+					throw this.client.methods.util.newError(this.msg.language.get('COMMANDMESSAGE_MISSING'), 1);
+				} else {
+					return this.params;
+				}
+			} else if (this._currentUsage.type === 'required' && this.args[this.params.length] === undefined) {
+				this.args.splice(this.params.length, 1, null);
+				throw this.client.methods.util.newError(this._currentUsage.possibles.length === 1 ?
+					this.msg.language.get('COMMANDMESSAGE_MISSING_REQUIRED', this._currentUsage.possibles[0].name) :
+					this.msg.language.get('COMMANDMESSAGE_MISSING_OPTIONALS', this._currentUsage.possibles.map(poss => poss.name).join(', ')), 1);
+			} else if (this._currentUsage.possibles.length === 1) {
+				if (this.client.argResolver[this._currentUsage.possibles[0].type]) {
+					return this.client.argResolver[this._currentUsage.possibles[0].type](this.args[this.params.length], this._currentUsage, 0, this._repeat, this.msg)
+						.catch((err) => {
+							this.args.splice(this.params.length, 1, null);
+							throw this.client.methods.util.newError(err, 1);
+						})
+						.then((res) => {
+							if (res !== null) {
+								this.params.push(res);
+								return this.validateArgs();
+							}
+							this.args.splice(this.params.length, 0, undefined);
+							this.params.push(undefined);
+							return this.validateArgs();
+						});
+				}
+				this.client.emit('warn', 'Unknown Argument Type encountered');
+				this.params.push(undefined);
+				return this.validateArgs();
+			} else {
+				return this.multiPossibles(0, false);
+			}
+		}
+
+		/**
+		 * Validates and resolves args into parameters, when multiple types of usage is defined
+		 * @since 0.0.1
+		 * @param {number} possible The id of the possible usage currently being checked
+		 * @param {boolean} validated Escapes the recursive function if the previous iteration validated the arg into a parameter
+		 * @private
+		 * @returns {any[]} The resolved parameters
+		 */
+		async multiPossibles(possible, validated) {
+			if (validated) {
+				return this.validateArgs();
+			} else if (possible >= this._currentUsage.possibles.length) {
+				if (this._currentUsage.type === 'optional' && !this._repeat) {
+					this.args.splice(this.params.length, 0, undefined);
+					this.params.push(undefined);
+					return this.validateArgs();
+				}
+				this.args.splice(this.params.length, 1, null);
+				throw this.client.methods.util.newError(this.msg.language.get('COMMANDMESSAGE_NOMATCH', this._currentUsage.possibles.map(poss => poss.name).join(', ')), 1);
+			} else if (this.client.argResolver[this._currentUsage.possibles[possible].type]) {
+				return this.client.argResolver[this._currentUsage.possibles[possible].type](this.args[this.params.length], this._currentUsage, possible, this._repeat, this.msg)
+					.then((res) => {
+						if (res !== null) {
+							this.params.push(res);
+							return this.multiPossibles(++possible, true);
+						}
+						return this.multiPossibles(++possible, validated);
+					})
+					.catch(() => this.multiPossibles(++possible, validated));
+			} else {
+				this.client.emit('warn', 'Unknown Argument Type encountered');
+				return this.multiPossibles(++possible, validated);
+			}
+		}
+
+		/**
 		 * Parses a message into string args
 		 * @since 0.0.1
 		 * @param {KlasaMessage} msg this message
