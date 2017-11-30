b5b5f16b25c26f162ef67722117903c0baed6c8f
AoDude <bdistin@gmail.com>
2017-11-30 09:53:14 -0600

    avoid getting config 2x

--- src/inhibitors/requiredConfigs.js
+++ src/inhibitors/requiredConfigs.js
@@ -6,7 +6,10 @@ module.exports = class extends Inhibitor {
 		if (cmd.requiredConfigs.length === 0) return;
 		if (msg.channel.type !== 'text') return;
 		const configs = msg.guildConfigs;
-		const requiredConfigs = cmd.requiredConfigs.filter(config => configs.get(config) !== undefined && configs.get(config) !== null);
+		const requiredConfigs = cmd.requiredConfigs.filter(config => {
+			const thisConfig = configs.get(config);
+			return thisConfig !== undefined && thisConfig !== null;
+		});
 		if (requiredConfigs.length > 0) throw msg.language.get('INHIBITOR_REQUIRED_CONFIGS', requiredConfigs);
 		return;
 	}
