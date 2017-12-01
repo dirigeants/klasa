523eac502bd9867527959a73334aaba20f216d83
bdistin <bdistin@gmail.com>
2017-11-30 21:59:04 -0600

    Fix docs indentation in KlasaUser

--- src/lib/extensions/KlasaUser.js
+++ src/lib/extensions/KlasaUser.js
@@ -2,9 +2,9 @@ const { Structures } = require('discord.js');
 
 module.exports = Structures.extend('User', User => {
 	/**
-     * Klasa's Extended User
-     * @extends external:User
-     */
+	 * Klasa's Extended User
+	 * @extends external:User
+	 */
 	class KlasaUser extends User {
 
 		constructor(...args) {
