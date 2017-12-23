7584e6d26ed001a263d1165356e46ba74dddd130
Unknown <bdistin@gmail.com>
2017-12-23 11:42:30 -0600

    update emoji regex to valid name lengths

--- src/lib/parsers/Resolver.js
+++ src/lib/parsers/Resolver.js
@@ -193,7 +193,7 @@ class Resolver {
 Resolver.regex = {
 	userOrMember: /^(?:<@!?)?(\d{17,19})>?$/,
 	channel: /^(?:<#)?(\d{17,19})>?$/,
-	emoji: /^(?:<a?:\w+:)?(\d{17,19})>?$/,
+	emoji: /^(?:<a?:\w{2,32}:)?(\d{17,19})>?$/,
 	role: /^(?:<@&)?(\d{17,19})>?$/,
 	snowflake: /^(\d{17,19})$/
 };
