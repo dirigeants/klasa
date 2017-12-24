90221c5ced4d0a6f89b7d76713d9705bd204364f
Unknown <bdistin@gmail.com>
2017-12-24 10:29:46 -0600

    Small code cleanup

--- src/monitors/commandHandler.js
+++ src/monitors/commandHandler.js
@@ -12,7 +12,7 @@ module.exports = class extends Monitor {
 
 	async run(msg) {
 		if (this.client.user.bot && msg.guild && !msg.guild.me) await msg.guild.members.fetch(this.client.user);
-		if (msg.guild && !msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES')) return;
+		if (msg.guild && !msg.channel.postable) return;
 		if (msg.content === this.client.user.toString() || (msg.guild && msg.content === msg.guild.me.toString())) {
 			msg.sendMessage(Array.isArray(msg.guildConfigs.prefix) ? msg.guildConfigs.prefix.map(prefix => `\`${prefix}\``).join(', ') : `\`${msg.guildConfigs.prefix}\``);
 			return;
