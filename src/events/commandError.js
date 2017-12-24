dd8540199581f522188260f48fdeb02f01490037
Unknown <bdistin@gmail.com>
2017-12-24 12:41:15 -0600

    we don't want to emit friendly string errors

--- src/events/commandError.js
+++ src/events/commandError.js
@@ -4,8 +4,7 @@ const { join } = require('path');
 module.exports = class extends Event {
 
 	run(msg, command, params, error) {
-		this.client.emit('wtf', `[COMMAND] ${join(command.dir, ...command.file)}\n${error ?
-			error.stack ? error.stack : error : 'Unknown error'}`);
+		if (error instanceof Error) this.client.emit('wtf', `[COMMAND] ${join(command.dir, ...command.file)}\n${error.stack ? error.stack : error}`);
 		if (error.message) msg.sendCode('JSON', error.message).catch(err => this.client.emit('wtf', err));
 		else msg.sendMessage(error).catch(err => this.client.emit('wtf', err));
 	}
