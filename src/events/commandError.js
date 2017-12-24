3f09cff94ba96ae6f51ca9abc4f94352a8bfe903
Unknown <bdistin@gmail.com>
2017-12-24 12:34:01 -0600

    better commandError

--- src/events/commandError.js
+++ src/events/commandError.js
@@ -1,10 +1,12 @@
 const { Event } = require('klasa');
+const { join } = require('path');
 
 module.exports = class extends Event {
 
 	run(msg, command, params, error) {
-		if (error.stack) this.client.emit('wtf', error.stack);
-		else if (error.message) msg.sendCode('JSON', error.message).catch(err => this.client.emit('wtf', err));
+		this.client.emit('wtf', `[COMMAND] ${join(command.dir, ...command.file)}\n${error ?
+			error.stack ? error.stack : error : 'Unknown error'}`);
+		if (error.message) msg.sendCode('JSON', error.message).catch(err => this.client.emit('wtf', err));
 		else msg.sendMessage(error).catch(err => this.client.emit('wtf', err));
 	}
 
