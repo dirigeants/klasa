297d819f41fb026da305042a5c803e4a21203f22
Unknown <bdistin@gmail.com>
2017-12-24 12:43:38 -0600

    They really need a useless ternary eslint rule

--- src/events/commandError.js
+++ src/events/commandError.js
@@ -4,7 +4,7 @@ const { join } = require('path');
 module.exports = class extends Event {
 
 	run(msg, command, params, error) {
-		if (error instanceof Error) this.client.emit('wtf', `[COMMAND] ${join(command.dir, ...command.file)}\n${error.stack ? error.stack : error}`);
+		if (error instanceof Error) this.client.emit('wtf', `[COMMAND] ${join(command.dir, ...command.file)}\n${error.stack || error}`);
 		if (error.message) msg.sendCode('JSON', error.message).catch(err => this.client.emit('wtf', err));
 		else msg.sendMessage(error).catch(err => this.client.emit('wtf', err));
 	}
