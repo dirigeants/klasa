f905ecefb7a0a2e28a3b5bff094901f48e1a52f8
Unknown <bdistin@gmail.com>
2017-12-25 09:00:41 -0600

    Make transfer shard friendly

--- src/commands/Admin/transfer.js
+++ src/commands/Admin/transfer.js
@@ -17,15 +17,20 @@ module.exports = class extends Command {
 		const file = piece.type === 'command' ? join(...piece.file) : piece.file;
 		const fileLocation = resolve(this.client.coreBaseDir, `${piece.type}s`, file);
 		await fs.access(fileLocation).catch(() => { throw msg.language.get('COMMAND_TRANSFER_ERROR'); });
-		return fs.copy(fileLocation, resolve(this.client.clientBaseDir, `${piece.type}s`, file))
-			.then(() => {
-				this.client[`${piece.type}s`].load(resolve(this.client.clientBaseDir, `${piece.type}s`), piece.file);
-				return msg.sendMessage(msg.language.get('COMMAND_TRANSFER_SUCCESS', piece.type, piece.name));
-			})
-			.catch((err) => {
-				this.client.emit('error', err.stack);
-				return msg.sendMessage(msg.language.get('COMMAND_TRANSFER_FAILED', piece.type, piece.name));
-			});
+		try {
+			const newFileLocation = resolve(this.client.clientBaseDir, `${piece.type}s`);
+			await fs.copy(fileLocation, join(newFileLocation, file));
+			this.client[`${piece.type}s`].load(newFileLocation, piece.file);
+			if (this.client.sharded) {
+				await this.client.shard.broadcastEval(`
+					if (this.shard.id !== ${this.client.shard.id}) this.${piece.type}s.load('${newFileLocation}', ${JSON.stringify(piece.file)});
+				`);
+			}
+			return msg.sendMessage(msg.language.get('COMMAND_TRANSFER_SUCCESS', piece.type, piece.name));
+		} catch (err) {
+			this.client.emit('error', err.stack);
+			return msg.sendMessage(msg.language.get('COMMAND_TRANSFER_FAILED', piece.type, piece.name));
+		}
 	}
 
 };
