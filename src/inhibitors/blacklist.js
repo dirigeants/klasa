46aa3292ed24702da9510483a0b0e9571ce68291
Ethoarx <moddersgithub@gmail.com>
2018-01-06 11:47:18 +0100

    did the requested changes

--- src/inhibitors/blacklist.js
+++ src/inhibitors/blacklist.js
@@ -3,7 +3,7 @@ const { Inhibitor } = require('klasa');
 module.exports = class extends Inhibitor {
 
 	async run(msg) {
-		if (msg.author === this.client.owner) return;
+		if (msg.author.id === this.client.options.ownerID) return;
 		if (this.client.configs.userBlacklist.includes(msg.author.id)) throw true;
 	}
 
