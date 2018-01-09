d7b0eaaffc2d136cc5ca257f092630378efa3f6a
Unknown <bdistin@gmail.com>
2018-01-08 20:27:49 -0600

    fix client.options.providers

--- src/lib/util/constants.js
+++ src/lib/util/constants.js
@@ -23,7 +23,7 @@ exports.DEFAULTS = {
 		language: 'en-US',
 		prefix: '!',
 		preserveConfigs: true,
-		provider: { engine: 'json' },
+		providers: { default: 'json' },
 		readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`,
 		typing: false,
 		customPromptDefaults: {
