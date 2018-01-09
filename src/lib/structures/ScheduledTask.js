c35946d40ea68ec3a2bda7f854280b3360535499
Unknown <bdistin@gmail.com>
2018-01-08 19:23:40 -0600

    ScheduledTask wip

--- src/lib/structures/ScheduledTask.js
+++ src/lib/structures/ScheduledTask.js
@@ -7,9 +7,8 @@ class ScheduledTask {
 
 	/**
 	 * @typedef  {Object} ScheduledTaskOptions
-	 * @property {string} id
+	 * @property {string} [id]
 	 * @property {Date} [time]
-	 * @property {boolean} [utc=false]
 	 * @property {string} [repeat]
 	 * @property {*} [data]
 	 */
@@ -47,16 +46,6 @@ class ScheduledTask {
 		/**
 		 * @type {string}
 		 */
-		this.id = options.id;
-
-		/**
-		 * @type {boolean}
-		 */
-		this.utc = Boolean(options.utc);
-
-		/**
-		 * @type {string}
-		 */
 		this.repeat = options.repeat;
 
 		/**
@@ -67,12 +56,19 @@ class ScheduledTask {
 		/**
 		 * @type {?Date}
 		 */
-		this.time = options.time instanceof Date ? options.time : null;
+		this.time = options.time instanceof Date ? options.time : this.recurring ? this.recurring.next() : null;
+
+		/**
+		 * @type {string}
+		 */
+		this.id = options.id || this.constructor._generateID(this.client, this.time);
 
 		/**
 		 * @type {*}
 		 */
 		this.data = options.data;
+
+		this.constructor.validate(this);
 	}
 
 	async run() {
@@ -85,51 +81,26 @@ class ScheduledTask {
 		return this.update({ time: this.recurring.next() });
 	}
 
-	// _parseRecurringDaily(options) {
-	// 	if (!Array.isArray(options)) return [];
-	// 	for (const time of options) {
-	// 		if (typeof time !== 'string') continue;
-	// 	}
-	// }
+	update({ repeat, time, data } = {}) {
+		if (repeat) {
+			this.repeat = repeat;
+			this.recurring = new Cron(repeat);
+			this.time = this.recurring.next();
+		}
+		if (time) this.time = time;
+		if (data) this.data = data;
+		this.store.sync(this);
+	}
 
-	// _parseMonthObject(time, date) {
-	// 	const dates = new Collection();
-	// 	for (const key in time) {
-	// 		if (!/\w\d{1,2}/.test(key)) throw new TypeError('[INVALID_DAY] The date should match the RegExp \\w\\d{1,2}');
-	// 		const day = parseInt(key.slice(1));
-	// 		if (day < 1 || day > 31) throw new Error('[INVALID_DAY_NUMBER] The day must be between 1 and 31');
-	// 		dates.set(day, time[day].map(timestamp => this._parseHour(timestamp, new Date(date))));
-	// 	}
-	// }
+	static _generateID(client, time) {
+		return Buffer.from(`${time}${client.sharded ? client.shard.id : ''}`).toString('hex');
+	}
 
-	// /**
-	//  * Parse recurring weeks
-	//  * @since 0.5.0
-	//  * @param {Object} time The time object
-	//  * @param {Date} date The date instance
-	//  * @returns {RecurringWeek}
-	//  */
-	// _parseWeekObject(time, date) {
-	// 	const obj = {};
-	// 	for (const day of RECURRING_WEEK_TYPES) {
-	// 		if (day in time) obj[day] = time[day].map(timestamp => this._parseHour(timestamp, new Date(date)));
-	// 	}
-	// 	return obj;
-	// }
+	static validate(st) {
+		if (!this.task) throw new Error('invalid task');
+		if (!st.time) throw new Error('time or repeat option required');
+	}
 
-	// /**
-	//  * Parse the hour
-	//  * @since 0.5.0
-	//  * @param {string} time The time to parse
-	//  * @param {Date} date The date instance
-	//  * @returns {Date}
-	//  */
-	// _parseHour(time, date) {
-	// 	if (typeof time !== 'string') throw new TypeError('[INVALID_HOUR] The hour should be a string');
-	// 	const [, hours, minutes, seconds] = /(\d{1,2})?:?(\d{1,2})?:?(\d{1,2})?/.exec(time);
-	// 	date[this.utc ? 'setUTCHours' : 'setHours'](hours, minutes, seconds);
-	// 	return date;
-	// }
 
 }
 
