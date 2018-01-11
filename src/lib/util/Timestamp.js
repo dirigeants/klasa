d859763dd061afda46ca2931a01402bf177877a1
kyraNET <kyradiscord@gmail.com>
2018-01-11 21:39:52 +0100

    Fixed token MM from timestamp not filling single-digit numbers with zero

--- src/lib/util/Timestamp.js
+++ src/lib/util/Timestamp.js
@@ -179,8 +179,8 @@ class Timestamp {
 			case 'YYY':
 			case 'YYYY': return String(time.getFullYear());
 			case 'Q': return String((time.getMonth() + 1) / 3);
-			case 'M':
-			case 'MM': return String(time.getMonth() + 1);
+			case 'M': return String(time.getMonth() + 1);
+			case 'MM': return String(time.getMonth() + 1).padStart(2, '0');
 			case 'MMM':
 			case 'MMMM': return months[time.getMonth()];
 			case 'D': return String(time.getDate());
