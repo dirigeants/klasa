a1e0bfedd0c46664ab4ae568d7eac6b93a82fc4c
Unknown <bdistin@gmail.com>
2018-01-16 17:14:45 -0600

    docs fixes

--- src/lib/usage/ParsedUsage.js
+++ src/lib/usage/ParsedUsage.js
@@ -80,7 +80,7 @@ class ParsedUsage {
 
 	/**
 	 * Customizes the response of an argument if it fails resolution.
-	 * @param {string} name The type of the usage argument
+	 * @param {string} name The name of the usage argument
 	 * @param {string|Function} response The custom response or i18n function
 	 * @returns {ParsedUsage}
 	 * @chainable
