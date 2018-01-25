cd114cce489135ec24e41e7748d17ff43333ca90
Unknown <bdistin@gmail.com>
2018-01-25 10:31:37 -0600

    update deploy to use new docs on all (code) branch pushes

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -3,30 +3,33 @@
 
 set -e
 
-if [ "$TRAVIS_BRANCH" != "master" -o -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo -e "Not building for a non master branch push - building without deploying."
+if [ -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo -e "Not building for a non branch push - building without deploying."
   npm run docs
+  npm run newdocs
   exit 0
 fi
 
-echo -e "Building for a master branch push - building and deploying."
+echo -e "Building for a branch push - building and deploying."
 
 REPO=$(git config remote.origin.url)
 SHA=$(git rev-parse --verify HEAD)
 
-TARGET_BRANCH="gh-pages"
-git clone $REPO dist -b $TARGET_BRANCH
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  TARGET_BRANCH="gh-pages"
+  git clone $REPO dist -b $TARGET_BRANCH
 
-npm run docs
+  npm run docs
 
-rsync -vau docs/ dist/
+  rsync -vau docs/ dist/
 
-cd dist
-git add --all .
-git config user.name "Travis CI"
-git config user.email "${COMMIT_EMAIL}"
-git commit -m "Docs build: ${SHA}" || true
-git push "https://${GH_TOKEN}@${GH_REF}" $TARGET_BRANCH
+  cd dist
+  git add --all .
+  git config user.name "Travis CI"
+  git config user.email "${COMMIT_EMAIL}"
+  git commit -m "Docs build: ${SHA}" || true
+  git push "https://${GH_TOKEN}@${GH_REF}" $TARGET_BRANCH
+fi
 
 #new docs
 
