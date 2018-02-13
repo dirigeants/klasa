1c2ea6f274d21381edd56cb621ee07eaaec86cb6
bdistin <bdistin@gmail.com>
2018-02-13 11:13:58 -0600

    x2

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -3,7 +3,7 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	async run(old, msg) {
-		if (!this.client.options.cmdEditing || old.content !== msg.content) return;
+		if (!this.client.options.cmdEditing || old.content === msg.content) return;
 		const commandHandler = this.client.monitors.get('commandHandler');
 		if (!commandHandler || !commandHandler.shouldRun(msg)) return;
 		try {
