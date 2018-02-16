dbe66da930f631ee682226955d7784f4fe1000ed
kyraNET <kyradiscord@gmail.com>
2018-02-16 17:15:57 +0100

    Shard support

--- src/commands/Admin/load.js
+++ src/commands/Admin/load.js
@@ -14,18 +14,25 @@ module.exports = class extends Command {
 		this.regExp = /\\\\?|\//g;
 	}
 
-	async run(msg, [core, store, path]) {
-		if (!path.endsWith('.js')) path += '.js';
+	async run(msg, [_core, store, _path]) {
+		if (!path.endsWith('.js')) _path += '.js';
+		const path = _path.split(this.regExp);
+		const core = Boolean(_core);
 		const timer = new Stopwatch();
-		const piece = store.load(path.split(this.regExp), Boolean(core));
+		const piece = store.load(path, core);
 
 		try {
 			if (!piece) throw msg.language.get('COMMAND_LOAD_FAIL');
 			await piece.init();
+			if (this.client.shard) {
+				await this.client.shard.broadcastEval(`
+					if (this.shard.id !== ${this.client.shard.id}) this.${piece.store}.load(${JSON.stringify(path)}, ${core});
+				`);
+			}
 			return msg.sendMessage('COMMAND_LOAD', timer.stop(), store.name, piece.name);
 		} catch (error) {
 			timer.stop();
-			throw msg.language.get('COMMAND_LOAD_ERROR', store.name, piece ? piece.name : path, error);
+			throw msg.language.get('COMMAND_LOAD_ERROR', store.name, piece ? piece.name : path.join('/'), error);
 		}
 	}
 
