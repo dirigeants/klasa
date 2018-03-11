c4032e2cd432178fe8c767c2584c505343534fb4
Yukine <DevYukine@gmx.de>
2018-03-11 21:08:05 +0100

    Update Configuration.js

--- src/lib/settings/Configuration.js
+++ src/lib/settings/Configuration.js
@@ -357,7 +357,7 @@ class Configuration {
 		if (this.gateway.sql) {
 			const keys = new Array(updated.length), values = new Array(updated.length);
 			console.log(updated)
-			for (let i = 0; i < updated.length; i++) [keys[i], values[i]] = updated[i];
+			for (let i = 0; i < updated.length; i++) { console.log(updated[i]) }
 			await this.gateway.provider.update(this.gateway.type, this.id, keys, values);
 		} else {
 			const updateObject = {};
