29c89126e7abd65b580b94b0a19860c81d6017c5
bdistin <bdistin@gmail.com>
2018-03-13 17:24:01 -0500

    sneaky for of [] is fine

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -3,9 +3,7 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(message) {
-		if (message.command && message.command.deletable && message.responses.length) {
-			for (const msg of message.responses) msg.delete();
-		}
+		if (message.command && message.command.deletable) for (const msg of message.responses) msg.delete();
 	}
 
 };
