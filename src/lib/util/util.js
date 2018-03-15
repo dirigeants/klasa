6c76c903dc3a673ffd823c3c0e29ac35bb4d2429
kyranet <kyradiscord@gmail.com>
2018-03-15 14:54:38 +0100

    Fixed edge case in Util.isClass

--- src/lib/util/util.js
+++ src/lib/util/util.js
@@ -152,6 +152,7 @@ class Util {
 	 */
 	static isClass(input) {
 		return typeof input === 'function' &&
+			typeof input.prototype === 'object' &&
 			input.toString().substring(0, 5) === 'class';
 	}
 
