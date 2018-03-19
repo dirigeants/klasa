0ed118789b3b3ffe4fa0dbecde4821829b2501eb
bdistin <bdistin@gmail.com>
2018-03-18 21:26:05 -0500

    update typings for last commit

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -965,7 +965,7 @@ declare module 'klasa' {
 		public max: number;
 		public regex: RegExp;
 
-		private static resolveLimit(limit: string, type: string): number;
+		private static resolveLimit(limit: string, type: string, limitType: string): number;
 	}
 
 	export class Tag {
