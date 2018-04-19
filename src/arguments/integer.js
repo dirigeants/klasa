126ccfaa1444c19642ffd6ada535a1ca9c8b43f8
bdistin <bdistin@gmail.com>
2018-04-19 14:59:56 -0500

    fix integer

--- src/arguments/integer.js
+++ src/arguments/integer.js
@@ -10,7 +10,7 @@ module.exports = class extends Argument {
 		const { min, max } = possible;
 		const number = parseInt(arg);
 		if (!Number.isInteger(number)) throw (msg.language || this.client.languages.default).get('RESOLVER_INVALID_INT', possible.name);
-		return this.constructor.minOrMax(this.client, arg, min, max, possible, msg) ? arg : null;
+		return this.constructor.minOrMax(this.client, number, min, max, possible, msg) ? number : null;
 	}
 
 };
