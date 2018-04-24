55288b72d51b1e4d32a7c6bcc8755d1733673193
Yukine <blazylp@gmail.com>
2018-04-24 17:40:53 +0200

    temporary fix the configUpdateEntry event

--- src/events/configUpdateEntry.js
+++ src/events/configUpdateEntry.js
@@ -2,16 +2,16 @@ const { Event } = require('klasa');
 
 module.exports = class extends Event {
 
-	run(configs) {
+	run(oldConf, newConf) {
 		if (!this.client.shard) return;
-		if (configs.gateway.type === 'users') {
+		if (newConf.gateway.type === 'users') {
 			this.client.shard.broadcastEval(`
 				if (this.shard.id !== ${this.client.shard.id}) {
-					const user = this.users.get('${configs.id}');
+					const user = this.users.get('${newConf.id}');
 					if (user) user.configs.sync();
 				}
 			`);
-		} else if (configs.gateway.type === 'clientStorage') {
+		} else if (newConf.gateway.type === 'clientStorage') {
 			this.client.shard.broadcastEval(`
 				if (this.shard.id !== ${this.client.shard.id}) {
 					this.configs.sync();
