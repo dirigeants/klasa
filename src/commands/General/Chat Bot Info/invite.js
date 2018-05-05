ad139577368ebc2a3f7d57153cae40673cb866a2
Tylertron1998 <34944514+Tylertron1998@users.noreply.github.com>
2018-05-05 21:21:59 +0100

    Update invite.js (#294)
    * Update invite.js
    
    Update invite to only work for non-owners when application is public.
    
    * Update invite.js

--- src/commands/General/Chat Bot Info/invite.js
+++ src/commands/General/Chat Bot Info/invite.js
@@ -16,4 +16,8 @@ module.exports = class extends Command {
 		return message.sendMessage(message.language.get('COMMAND_INVITE', this.client));
 	}
 
+	async init() {
+		if (this.client.application && !this.client.application.botPublic) this.permissionLevel = 10;
+	}
+
 };
