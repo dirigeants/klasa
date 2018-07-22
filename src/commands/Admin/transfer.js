9088a63ce31d3352c122a9c6c9a6eee1c1deb0ea
bdistin <bdistin@gmail.com>
2018-07-22 16:41:20 -0500

    Multi Core Stores (#365)
    * [WIP] Multi-Core-Stores
    
    * naming
    
    * bug fixed
    
    * more naming
    
    * whoops
    
    * whoops
    
    * do plugin directories sequentially
    
    * whoops
    
    * whoops
    
    * docs: Updated typings and minor JSDoc fixes
    
    * oops
    
    * fix language core defaults

--- src/commands/Admin/transfer.js
+++ src/commands/Admin/transfer.js
@@ -15,14 +15,14 @@ module.exports = class extends Command {
 
 	async run(message, [piece]) {
 		const file = join(...piece.file);
-		const fileLocation = resolve(piece.store.coreDir, file);
+		const fileLocation = resolve(piece.dir, file);
 		await fs.access(fileLocation).catch(() => { throw message.language.get('COMMAND_TRANSFER_ERROR'); });
 		try {
-			await fs.copy(fileLocation, join(piece.store.userDir, file));
-			piece.store.load(piece.file);
+			await fs.copy(fileLocation, join(piece.store.userDirectory, file));
+			piece.store.load(piece.store.userDirectory, piece.file);
 			if (this.client.shard) {
 				await this.client.shard.broadcastEval(`
-					if (this.shard.id !== ${this.client.shard.id}) this.${piece.store}.load(${JSON.stringify(piece.file)});
+					if (this.shard.id !== ${this.client.shard.id}) this.${piece.store}.load(${piece.store.userDirectory}, ${JSON.stringify(piece.file)});
 				`);
 			}
 			return message.sendMessage(message.language.get('COMMAND_TRANSFER_SUCCESS', piece.type, piece.name));
