50ef2d0369fd43f9f73abc2e13f3f244d41c14bb
Faith <tba28012@hotmail.com>
2018-08-06 14:02:55 -0400

    change checkNumber to be like channel

--- src/lib/settings/types/Number.js
+++ src/lib/settings/types/Number.js
@@ -4,7 +4,6 @@ module.exports = class extends Type {
 
 	async resolve(data, piece, guild) {
 		const number = this.checkNumber(data, piece, guild);
-		if (isNaN(number)) throw (guild ? guild.languauge : this.client.languages.default).get('RESOLVER_INVALID_FLOAT', piece.key);
 		return this.constructor.minOrMax(this.client, number, guild, piece) ? number : true;
 	}
 
@@ -12,12 +11,18 @@ module.exports = class extends Type {
 		const type = piece.type.toLowerCase();
 		let numb;
 		switch (type) {
-			case 'number': return Number(data);
+			case 'number':
+				numb = Number(data);
+				if (!isNaN(numb)) return numb;
+				throw (guild ? guild.languauge : this.client.languages.default).get('RESOLVER_INVALID_FLOAT', piece.key);
 			case 'integer':
 				numb = parseInt(data);
 				if (Number.isInteger(numb)) return numb;
 				throw (guild ? guild.language : this.client.languages.default).get('RESOLVER_INVALID_INT', piece.key);
-			case 'float': return parseFloat(data);
+			case 'float':
+				numb = parseFloat(data);
+				if (!isNaN(numb)) return numb;
+				throw (guild ? guild.languauge : this.client.languages.default).get('RESOLVER_INVALID_FLOAT', piece.key);
 		}
 		return null;
 	}
