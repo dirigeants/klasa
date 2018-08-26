b0ead4ce3252fe38548ccdfab93451de2c37cad5
Unknown <bdistin@gmail.com>
2018-08-26 12:51:13 -0500

    docs

--- src/lib/util/RateLimit.js
+++ src/lib/util/RateLimit.js
@@ -1,24 +1,46 @@
+/**
+ * Handels generic ratelimits such as for {@link Command#cooldown}s
+ */
 class RateLimit {
 
+	/**
+	 * @since 0.5.0
+	 * @param {RateLimitManager} manager The RateLimitManager for this Ratelimit
+	 */
 	constructor(manager) {
+		/**
+		 * The RateLimitManager for this Ratelimit
+		 * @since 0.5.0
+		 * @type {RateLimitManager}
+		 */
 		this.manager = manager;
+
 		this.reset();
 	}
 
+	/**
+	 * If this RateLimit is limited or not
+	 * @since 0.5.0
+	 * @type {boolean}
+	 */
 	get limited() {
 		return !(this.remaining > 0 || this.expired);
 	}
 
+	/**
+	 * If this RateLimit is expired or not, allowing the bucket to be reset
+	 * @since 0.5.0
+	 * @type {boolean}
+	 */
 	get expired() {
 		return Date.now() >= this.resetTime;
 	}
 
-	reset() {
-		this.remaining = this.manager.bucket;
-		this.resetTime = Date.now() + this.manager.cooldown;
-		return this;
-	}
 
+	/**
+	 * Drips the RateLimit bucket
+	 * @returns {this}
+	 */
 	drip() {
 		if (this.limited) throw new Error('Ratelimited');
 		if (this.expired) this.reset();
@@ -27,6 +49,34 @@ class RateLimit {
 		return this;
 	}
 
+	/**
+	 * Resets the RateLimit back to it's full state
+	 * @returns {this}
+	 */
+	reset() {
+		/**
+		 * The remaining times this RateLimit can be dripped before the RateLimit bucket is empty
+		 * @since 0.5.0
+		 * @type {number}
+		 * @private
+		 */
+		this.remaining = this.manager.bucket;
+
+		/**
+		 * When this RateLimit is reset back to a full state
+		 * @since 0.5.0
+		 * @type {number}
+		 * @private
+		 */
+		this.resetTime = Date.now() + this.manager.cooldown;
+
+		return this;
+	}
+
+	/**
+	 * Returns a drip back into the RateLimit bucket
+	 * @returns {this}
+	 */
 	undrip() {
 		this.count = Math.min(this.remaining + 1, this.manager.bucket);
 		return this;
--- src/lib/util/RateLimitManager.js
+++ src/lib/util/RateLimitManager.js
@@ -1,18 +1,74 @@
 const { Collection } = require('discord.js');
 const RateLimit = require('./RateLimit');
 
+/**
+ * Manages {@link RateLimit}s
+ * @extends {external:Collection}
+ */
 class RateLimitManager extends Collection {
 
+	/**
+	 * @since 0.5.0
+	 * @param {number} bucket The amount of times a RateLimit can drip before it's limited
+	 * @param {number} cooldown The amount of time in seconds for the RateLimit to reset
+	 */
 	constructor(bucket, cooldown) {
 		super();
 
+		/**
+		 * The sweep interval for this RateLimitManager
+		 * @since 0.5.0
+		 * @type {?interval}
+		 * @private
+		 */
 		this.sweepInterval = null;
 
+		/**
+		 * The amount of times a RateLimit from this manager can drip before it's limited
+		 * @since 0.5.0
+		 * @type {number}
+		 */
 		this.bucket = bucket;
 
+		/**
+		 * The amount of time in seconds for a RateLimit from this manager to reset
+		 * @since 0.5.0
+		 * @type {number}
+		 */
 		this.cooldown = cooldown * 1000;
 	}
 
+	/**
+	 * Creates a {@link RateLimit} for this manager
+	 * @param {string} id The id the RateLimit belongs to
+	 * @returns {RateLimit}
+	 */
+	create(id) {
+		const rateLimit = new RateLimit(this);
+		this.set(id, rateLimit);
+		return rateLimit;
+	}
+
+	/**
+	 * Wraps {@link external:Collection}'s set method to set interval to sweep inactive RateLimits
+	 * @param {string} id The id the RateLimit belongs to
+	 * @param {RateLimit} rateLimit The this for the sweep
+	 * @returns {number}
+	 * @private
+	 */
+	set(id, rateLimit) {
+		if (rateLimit instanceof RateLimit) throw new TypeError('Invalid RateLimit');
+		if (!this.sweepInterval) this.sweepInterval = setInterval(this.sweep.bind(this), 30000);
+		return super.set(id, rateLimit);
+	}
+
+	/**
+	 * Wraps {@link external:Collection}'s sweep method to clear the interval when this manager is empty
+	 * @param {Function} fn The filter function
+	 * @param {any} thisArg The this for the sweep
+	 * @returns {number}
+	 * @private
+	 */
 	sweep(fn = rl => rl.expired, thisArg) {
 		const amount = super.sweep(fn, thisArg);
 
@@ -22,16 +78,6 @@ class RateLimitManager extends Collection {
 		}
 
 		return amount;
-	}
-
-	set(id, rateLimit) {
-		if (rateLimit instanceof RateLimit) throw new TypeError('Invalid RateLimit');
-		if (!this.sweepInterval) this.sweepInterval = setInterval(this.sweep.bind(this), 30000);
-		return super.set(id, rateLimit);
-	}
-
-	create(id) {
-		return this.set(id, new RateLimit(this));
 	}
 
 }
