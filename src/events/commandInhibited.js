9f789a45541e621a94f32fc1e4d948bc35f8d2bc
Unknown <bdistin@gmail.com>
2018-08-26 19:12:40 -0500

    also need to undrip if commandInhibited

--- src/events/commandInhibited.js
+++ src/events/commandInhibited.js
@@ -3,6 +3,10 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(message, command, response) {
+		if (command.cooldown !== 0) {
+			const existing = command.cooldowns.get(message.levelID);
+			if (existing) existing.undrip();
+		}
 		if (response && response.length) message.sendMessage(response);
 	}
 
