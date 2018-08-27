bbd3c19be248ddc109fac1ce765d77ea71006f6e
Unknown <bdistin@gmail.com>
2018-08-26 19:32:49 -0500

    actually you can't do that
    because if a cooldown inhibits, it undoes the cooldown for one more use... But now we have commands going on cooldown when commands are inhibited, which can also be bad...

--- src/events/commandInhibited.js
+++ src/events/commandInhibited.js
@@ -3,10 +3,6 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(message, command, response) {
-		if (command.cooldown !== 0) {
-			const existing = command.cooldowns.get(message.levelID);
-			if (existing) existing.undrip();
-		}
 		if (response && response.length) message.sendMessage(response);
 	}
 
