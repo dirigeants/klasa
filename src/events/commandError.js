ebf0161e204f042693927e32811aace8ac79ae63
Unknown <bdistin@gmail.com>
2018-08-26 19:58:14 -0500

    forgot to revert this

--- src/events/commandError.js
+++ src/events/commandError.js
@@ -3,10 +3,6 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(message, command, params, error) {
-		if (command.cooldown !== 0) {
-			const existing = command.cooldowns.get(message.levelID);
-			if (existing) existing.undrip();
-		}
 		if (error instanceof Error) this.client.emit('wtf', `[COMMAND] ${command.path}\n${error.stack || error}`);
 		if (error.message) message.sendCode('JSON', error.message).catch(err => this.client.emit('wtf', err));
 		else message.sendMessage(error).catch(err => this.client.emit('wtf', err));
