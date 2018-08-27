099a9ae3cd24ba1d04bdbef51f3099c112a91c4a
Unknown <bdistin@gmail.com>
2018-08-26 20:10:26 -0500

    fix spacing

--- src/finalizers/commandCooldown.js
+++ src/finalizers/commandCooldown.js
@@ -7,7 +7,6 @@ module.exports = class extends Finalizer {
 		if (message.command.cooldown <= 0) return;
 
 		const id = message.levelID;
-
 		const rateLimit = message.command.cooldowns.get(id) || message.command.cooldowns.create(id);
 
 		try {
