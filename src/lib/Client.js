f395fc7ce869e55f35a11a60c51e3dbdcfe20bac
kyraNET <kyradiscord@gmail.com>
2018-08-30 17:54:55 +0200

    src: Remove scheduledtask serializer

--- src/lib/Client.js
+++ src/lib/Client.js
@@ -517,7 +517,7 @@ KlasaClient.defaultUserSchema = new Schema();
 KlasaClient.defaultClientSchema = new Schema()
 	.add('userBlacklist', 'user', { array: true, configurable: true })
 	.add('guildBlacklist', 'guild', { array: true, configurable: true })
-	.add('schedules', 'task', { array: true, configurable: false });
+	.add('schedules', 'any', { array: true, configurable: false });
 
 /**
  * Emitted when Klasa is fully ready and initialized.
