6710560dbc185f0308391b83fe5e7ffd00d781f9
kyranet <kyradiscord@gmail.com>
2018-09-07 12:35:22 +0200

    feat: Added `KlasaClientOptions.createPiecesFolders`

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -44,7 +44,7 @@ declare module 'klasa' {
 //#region Classes
 
 	export class KlasaClient extends Client {
-		public constructor(options?: KlasaClientOptions & ClientOptions);
+		public constructor(options?: KlasaClientOptions);
 		public readonly invite: string;
 		public readonly owner: KlasaUser | null;
 		public options: KlasaClientOptions & ClientOptions;
@@ -1283,6 +1283,7 @@ declare module 'klasa' {
 		commandMessageLifetime?: number;
 		console?: KlasaConsoleConfig;
 		consoleEvents?: KlasaConsoleEvents;
+		createPiecesFolders?: boolean;
 		customPromptDefaults?: KlasaCustomPromptDefaults;
 		disabledCorePieces?: string[];
 		gateways?: KlasaGatewaysOptions;
