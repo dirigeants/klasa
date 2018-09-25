b917614541c29d632066453fe6d44b922616cf24
Skillz4Killz <gankstarsleague@gmail.com>
2018-09-25 00:03:27 -0400

    Update requiredSettings.js

--- src/inhibitors/requiredSettings.js
+++ src/inhibitors/requiredSettings.js
@@ -6,7 +6,7 @@ module.exports = class extends Inhibitor {
 		if (!command.requiredSettings.length) return;
 		
 		// eslint-disable-next-line eqeqeq
-		const requiredSettings = command.requiredSettings.filter(setting => message[setting[0].settings.get(setting[1]) == null);
+		const requiredSettings = command.requiredSettings.filter(setting => message[setting[0]].settings.get(setting[1]) == null);
 		if (requiredSettings.length) throw message.language.get('INHIBITOR_REQUIRED_SETTINGS', requiredSettings, guildOrAuthor);
 	}
 
