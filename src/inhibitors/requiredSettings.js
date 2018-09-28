76aab1a85592c6b0d37252253e64cdd1fba6a3ff
Skillz4Killz <gankstarsleague@gmail.com>
2018-09-28 19:48:51 -0400

    Update requiredSettings.js

--- src/inhibitors/requiredSettings.js
+++ src/inhibitors/requiredSettings.js
@@ -5,16 +5,16 @@ module.exports = class extends Inhibitor {
 	async run(message, command) {
 		const gateways = Object.keys(command.requiredSettings);
 		if (!gateways.length) return;
-		
+
 		const missingSettings = [];
-		
+
 		for (const gateway of gateways) {
 			for (const setting of command.requireSettings[gateway]) {
 				if (message[gateway].settings.get(setting) == null) missingSettings.push([gateway, setting]);
 			}
 		}
 
-		if (requiredSettings.length) throw message.language.get('INHIBITOR_REQUIRED_SETTINGS', requiredSettings);
+		if (missingSettings.length) throw message.language.get('INHIBITOR_REQUIRED_SETTINGS', missingSettings);
 	}
 
 };
