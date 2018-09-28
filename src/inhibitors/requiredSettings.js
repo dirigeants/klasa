091d162e7872dcedc1a82301273646b295fda156
Skillz4Killz <gankstarsleague@gmail.com>
2018-09-28 19:12:12 -0400

    Update requiredSettings.js

--- src/inhibitors/requiredSettings.js
+++ src/inhibitors/requiredSettings.js
@@ -3,10 +3,17 @@ const { Inhibitor } = require('klasa');
 module.exports = class extends Inhibitor {
 
 	async run(message, command) {
-		if (!command.requiredSettings.length) return;
+		const gateways = Object.keys(command.requiredSettings);
+		if (!gateways.length) return;
 		
-		// eslint-disable-next-line eqeqeq
-		const requiredSettings = command.requiredSettings.filter(setting => message[setting[0]].settings.get(setting[1]) == null);
+		const missingSettings = [];
+		
+		for (const gateway of gateways) {
+			for (const setting of command.requireSettings[gateway]) {
+				if (message[gateway].settings.get(setting) == null) missingSettings.push([gateway, setting]);
+			}
+		}
+
 		if (requiredSettings.length) throw message.language.get('INHIBITOR_REQUIRED_SETTINGS', requiredSettings);
 	}
 
