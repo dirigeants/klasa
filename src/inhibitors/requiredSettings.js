f90b0b420e5ba40469c215a5d116a87a5028cecd
Skillz4Killz <gankstarsleague@gmail.com>
2018-10-04 12:33:43 -0400

    Requested changes

--- src/inhibitors/requiredSettings.js
+++ src/inhibitors/requiredSettings.js
@@ -3,12 +3,10 @@ const { Inhibitor } = require('klasa');
 module.exports = class extends Inhibitor {
 
 	async run(message, command) {
-		const gateways = Object.keys(command.requiredSettings);
-		if (!gateways.length) return;
-
 		const missingSettings = [];
 
 		for (const [gateway, settings] of Object.entries(command.requiredSettings)) {
+			if (gateway === 'guild' && message.channel.type !== 'text') continue;
 			for (const setting of settings) {
 				// eslint-disable-next-line eqeqeq
 				if (message[gateway].settings.get(setting) == null) missingSettings.push([gateway, setting]);
