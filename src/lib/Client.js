9b4ed72ae07d424e3928e3d9e9517567555385be
Kyra <kyranet@users.noreply.github.com>
2018-10-18 23:33:44 +0200

    fix: GuildBlacklist failing at parsing external guilds

--- src/lib/Client.js
+++ src/lib/Client.js
@@ -519,7 +519,7 @@ KlasaClient.defaultUserSchema = new Schema();
  */
 KlasaClient.defaultClientSchema = new Schema()
 	.add('userBlacklist', 'user', { array: true, configurable: true })
-	.add('guildBlacklist', 'guild', { array: true, configurable: true })
+	.add('guildBlacklist', 'string', { array: true, configurable: true })
 	.add('schedules', 'any', { array: true, configurable: false });
 
 /**
