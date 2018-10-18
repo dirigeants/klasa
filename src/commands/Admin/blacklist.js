fd3f1428546bdd09bf64a96b8d8d55fc3b984fa6
kyranet <kyradiscord@gmail.com>
2018-10-19 00:15:57 +0200

    fix: changes may not be ids

--- src/commands/Admin/blacklist.js
+++ src/commands/Admin/blacklist.js
@@ -17,14 +17,16 @@ module.exports = class extends Command {
 
 	async run(message, usersAndGuilds) {
 		const changes = [[], [], [], []];
+		const queries = [[], []];
 
 		for (const userOrGuild of new Set(usersAndGuilds)) {
 			const type = userOrGuild instanceof User ? 'user' : 'guild';
 			if (this.client.settings[`${type}Blacklist`].includes(userOrGuild.id || userOrGuild)) changes[this.terms.indexOf(`${type}sAdded`)].push(userOrGuild.name || userOrGuild.username || userOrGuild);
 			else changes[this.terms.indexOf(`${type}sRemoved`)].push(userOrGuild.name || userOrGuild.username || userOrGuild);
+			queries[Number(type === 'user')].push(userOrGuild.id || userOrGuild);
 		}
 
-		const { errors } = await this.client.settings.update([['userBlacklist', changes[0].concat(changes[1])], ['guildBlacklist', changes[2].concat(changes[3])]]);
+		const { errors } = await this.client.settings.update([['userBlacklist', queries[0]], ['guildBlacklist', queries[1]]]);
 		if (errors.length) throw String(errors[0]);
 
 		return message.sendLocale('COMMAND_BLACKLIST_SUCCESS', changes);
