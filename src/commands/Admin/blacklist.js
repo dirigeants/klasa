12f3d3fa5680358c9a511fc094fee3dd364d6e98
kyranet <kyradiscord@gmail.com>
2018-10-19 00:02:08 +0200

    fix: Make blacklist only do a single Settings#update

--- src/commands/Admin/blacklist.js
+++ src/commands/Admin/blacklist.js
@@ -11,20 +11,28 @@ module.exports = class extends Command {
 			usageDelim: ' ',
 			guarded: true
 		});
-
-		this.terms = ['usersAdded', 'usersRemoved', 'guildsAdded', 'guildsRemoved'];
 	}
 
 	async run(message, usersAndGuilds) {
-		usersAndGuilds = new Set(usersAndGuilds);
+		const users = [];
+		const guilds = [];
+
+		for (const userOrGuild of new Set(usersAndGuilds)) {
+			if (userOrGuild instanceof User) users.push(userOrGuild.id);
+			else guilds.push(typeof userOrGuild === 'string' ? userOrGuild : userOrGuild.id);
+		}
+
+		const { errors } = await this.client.settings.update([['userBlacklist', users], ['guildBlacklist', guilds]]);
+		if (errors.length) throw String(errors[0]);
+
 		const changes = [[], [], [], []];
-
-		for (const userOrGuild of usersAndGuilds) {
-			const type = userOrGuild instanceof User ? 'user' : 'guild';
-			await this.client.settings.update(`${type}Blacklist`, userOrGuild.id || userOrGuild, message.guild);
-
-			if (this.client.settings[`${type}Blacklist`].includes(userOrGuild.id || userOrGuild)) changes[this.terms.indexOf(`${type}sAdded`)].push(userOrGuild.name || userOrGuild.username || userOrGuild);
-			else changes[this.terms.indexOf(`${type}sRemoved`)].push(userOrGuild.name || userOrGuild.username || userOrGuild);
+		for (const user of users) {
+			if (this.client.settings.userBlacklist.includes(user)) changes[0].push(user);
+			else changes[1].push(user);
+		}
+		for (const guild of guilds) {
+			if (this.client.settings.guildBlacklist.includes(guild)) changes[2].push(guild);
+			else changes[3].push(guild);
 		}
 
 		return message.sendLocale('COMMAND_BLACKLIST_SUCCESS', changes);
