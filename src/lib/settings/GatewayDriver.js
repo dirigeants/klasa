9d8241d85a90507efa5f50c9c1402b820bc540c6
Kyra <kyradiscord@gmail.com>
2018-10-28 20:06:26 +0100

    refactor: Convert GatewayDriver into a Collection<string, Gateway> (#467)
    * refactor: Convert GatewayDriver into a Collection<string, Gateway>
    
    * fix: Better error message for gateway existence in register
    
    Co-Authored-By: kyranet <kyradiscord@gmail.com>

--- src/lib/settings/GatewayDriver.js
+++ src/lib/settings/GatewayDriver.js
@@ -1,11 +1,12 @@
 const Gateway = require('./Gateway');
 const Schema = require('./schema/Schema');
+const Collection = require('discord.js');
 
 /**
  * <warning>GatewayDriver is a singleton, use {@link KlasaClient#gateways} instead.</warning>
  * Gateway's driver to make new instances of it, with the purpose to handle different databases simultaneously.
  */
-class GatewayDriver {
+class GatewayDriver extends Collection {
 
 	/**
 	 * @typedef {Object} GatewayDriverRegisterOptions
@@ -19,6 +20,8 @@ class GatewayDriver {
 	 * @param {KlasaClient} client The Klasa client
 	 */
 	constructor(client) {
+		super();
+
 		/**
 		 * The client this GatewayDriver was created with.
 		 * @since 0.3.0
@@ -27,40 +30,6 @@ class GatewayDriver {
 		 * @readonly
 		 */
 		Object.defineProperty(this, 'client', { value: client });
-
-		/**
-		 * The register creation queue.
-		 * @since 0.5.0
-		 * @name GatewayDriver#_queue
-		 * @type {Array<Function>}
-		 * @readonly
-		 * @private
-		 */
-		Object.defineProperty(this, '_queue', { value: [] });
-
-		/**
-		 * All the gateways added
-		 * @type {Set<string>}
-		 */
-		this.keys = new Set();
-
-		/**
-		 * The Gateway that manages per-guild data
-		 * @type {?Gateway}
-		 */
-		this.guilds = null;
-
-		/**
-		 * The Gateway that manages per-user data
-		 * @type {?Gateway}
-		 */
-		this.users = null;
-
-		/**
-		 * The Gateway that manages per-client data
-		 * @type {?Gateway}
-		 */
-		this.clientStorage = null;
 	}
 
 
@@ -75,13 +44,11 @@ class GatewayDriver {
 	register(name, { provider = this.client.options.providers.default, schema = new Schema() } = {}) {
 		if (typeof name !== 'string') throw new TypeError('You must pass a name for your new gateway and it must be a string.');
 		if (!(schema instanceof Schema)) throw new TypeError('Schema must be a valid Schema instance.');
-		if (this.name !== undefined && this.name !== null) throw new Error(`The key '${name}' is either taken by another Gateway or reserved for GatewayDriver's functionality.`);
+		if (this.has(name)) throw new Error(`The key '${name}' is taken by another Gateway.`);
 
-		const gateway = new Gateway(this, name, schema, provider);
-		this.keys.add(name);
-		this[name] = gateway;
-		this._queue.push(gateway.init.bind(gateway));
 		if (!(name in this.client.options.gateways)) this.client.options.gateways[name] = {};
+		const gateway = new Gateway(this, name, schema, provider);
+		this.set(name, gateway);
 		return this;
 	}
 
@@ -90,33 +57,17 @@ class GatewayDriver {
 	 * @since 0.5.0
 	 */
 	async init() {
-		await Promise.all([...this._queue].map(fn => fn()));
-		this._queue.length = 0;
+		await Promise.all([...this.values()].map(gateway => gateway.init()));
 	}
 
 	/**
 	 * Sync all gateways
 	 * @since 0.5.0
-	 * @param {(string|string[])} input The arguments to pass to each Gateway#sync
+	 * @param {(string|string[])} [input] The arguments to pass to each Gateway#sync
 	 * @returns {Promise<Array<Gateway>>}
 	 */
 	sync(input) {
 		return Promise.all([...this].map(([key, gateway]) => gateway.sync(typeof input === 'undefined' ? this.client.options.gateways[key].syncArg : input)));
-	}
-
-	/**
-	 * Returns a new Iterator object that contains the values for each gateway contained in this driver.
-	 * @name @@iterator
-	 * @since 0.5.0
-	 * @method
-	 * @instance
-	 * @generator
-	 * @returns {Iterator<Array<string | Gateway>>}
-	 * @memberof GatewayDriver
-	 */
-
-	*[Symbol.iterator]() {
-		for (const key of this.keys) yield [key, this[key]];
 	}
 
 	/**
@@ -126,7 +77,6 @@ class GatewayDriver {
 	 */
 	toJSON() {
 		return {
-			keys: [...this.keys],
 			ready: this.ready,
 			...Object.assign({}, [...this].map(([key, value]) => ({ [key]: value.toJSON() })))
 		};
@@ -138,7 +88,7 @@ class GatewayDriver {
 	 * @returns {string}
 	 */
 	toString() {
-		return `GatewayDriver(${[...this.keys].join(', ')})`;
+		return `GatewayDriver(${[...this.keys()].join(', ')})`;
 	}
 
 }
