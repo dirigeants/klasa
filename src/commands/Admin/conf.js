a482909727f9dd2e31902adeedbd8403004cd737
Kyra <kyradiscord@gmail.com>
2018-10-28 23:51:16 +0100

    refactor: Upgrade API for Settings#update (#426)
    * refactor: No more _update
    
    * cleanup: More re-organization
    
    * src: More refactors
    
    * Settings#{list,resolveString} -> Settings#display
    
    * style: Rename constant name
    
    * refactor: Re-add Conf#check and reduce duplicated code in Settings#reset
    
    * fix: _resolveUpdateOverloads not parsing reset args correctly
    
    * fix: Synchronize the Settings entry on update or reset instead of returning wrong results
    
    * typings: Fixed QB types and updated everything
    
    * docs: Added missing jsdocs and refactored Settings#get
    
    * fix: conf and userconf always throwing on empty values
    
    * fix: Better Schema#get
    
    * fix: Settings#show using Map[key] instead of Map#get(key)
    
    * fix: Settings#_resolveUpdateOverloads not checking for the lack of a key
    
    * fix: conf command not passing all args for check
    
    * fix: Fixed multiple things
    
    * docs: Update guides to reflect latest changes
    
    * pr: Update changelog

--- src/commands/Admin/conf.js
+++ src/commands/Admin/conf.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
 			guarded: true,
 			subcommands: true,
 			description: language => language.get('COMMAND_CONF_SERVER_DESCRIPTION'),
-			usage: '<set|show|remove|reset> (key:key) (value:value) [...]',
+			usage: '<set|show|remove|reset> (key:key) (value:value)',
 			usageDelim: ' '
 		});
 
@@ -19,42 +19,43 @@ module.exports = class extends Command {
 				throw message.language.get('COMMAND_CONF_NOKEY');
 			})
 			.createCustomResolver('value', (arg, possible, message, [action]) => {
-				if (!['set', 'remove'].includes(action) || arg) return arg;
+				if (!['set', 'remove'].includes(action)) return null;
+				if (arg) return this.client.arguments.get('...string').run(arg, possible, message);
 				throw message.language.get('COMMAND_CONF_NOVALUE');
 			});
 	}
 
 	show(message, [key]) {
-		const path = this.client.gateways.get('guilds').getPath(key, { avoidUnconfigurable: true, errors: false, piece: null });
-		if (!path) return message.sendLocale('COMMAND_CONF_GET_NOEXT', [key]);
-		if (path.piece.type === 'Folder') {
+		const piece = this.client.gateways.get('guilds').schema.get(key || '');
+		if (!piece || piece.type === 'Folder' ? !piece.configurableKeys.length : !piece.configurable) return message.sendLocale('COMMAND_CONF_GET_NOEXT', [key]);
+		if (piece.type === 'Folder') {
 			return message.sendLocale('COMMAND_CONF_SERVER', [
 				key ? `: ${key.split('.').map(toTitleCase).join('/')}` : '',
-				codeBlock('asciidoc', message.guild.settings.list(message, path.piece))
+				codeBlock('asciidoc', message.guild.settings.display(message, piece))
 			]);
 		}
-		return message.sendLocale('COMMAND_CONF_GET', [path.piece.path, message.guild.settings.resolveString(message, path.piece)]);
+		return message.sendLocale('COMMAND_CONF_GET', [piece.path, message.guild.settings.display(message, piece)]);
 	}
 
-	async set(message, [key, ...valueToSet]) {
-		const status = await message.guild.settings.update(key, valueToSet.join(' '), message.guild, { avoidUnconfigurable: true, action: 'add' });
-		return this.check(message, key, status) || message.sendLocale('COMMAND_CONF_UPDATED', [key, message.guild.settings.resolveString(message, status.updated[0].piece)]);
+	async set(message, [key, valueToSet]) {
+		const piece = this.check(message, key, await message.guild.settings.update(key, valueToSet, { avoidUnconfigurable: true, action: 'add', guild: message.guild }));
+		return message.sendLocale('COMMAND_CONF_UPDATED', [key, message.guild.settings.display(message, piece)]);
 	}
 
-	async remove(message, [key, ...valueToRemove]) {
-		const status = await message.guild.settings.update(key, valueToRemove.join(' '), message.guild, { avoidUnconfigurable: true, action: 'remove' });
-		return this.check(message, key, status) || message.sendLocale('COMMAND_CONF_UPDATED', [key, message.guild.settings.resolveString(message, status.updated[0].piece)]);
+	async remove(message, [key, valueToRemove]) {
+		const piece = this.check(message, key, await message.guild.settings.update(key, valueToRemove, { avoidUnconfigurable: true, action: 'remove', guild: message.guild }));
+		return message.sendLocale('COMMAND_CONF_UPDATED', [key, message.guild.settings.display(message, piece)]);
 	}
 
 	async reset(message, [key]) {
-		const status = await message.guild.settings.reset(key, message.guild, true);
-		return this.check(message, key, status) || message.sendLocale('COMMAND_CONF_RESET', [key, message.guild.settings.resolveString(message, status.updated[0].piece)]);
+		const piece = this.check(message, key, await message.guild.settings.reset(key, { guild: message.guild }));
+		return message.sendLocale('COMMAND_CONF_RESET', [key, message.guild.settings.display(message, piece)]);
 	}
 
 	check(message, key, { errors, updated }) {
-		if (errors.length) return message.sendMessage(String(errors[0]));
-		if (!updated.length) return message.sendLocale('COMMAND_CONF_NOCHANGE', [key]);
-		return null;
+		if (errors.length) throw String(errors[0]);
+		if (!updated.length) throw message.language.get('COMMAND_CONF_NOCHANGE', key);
+		return updated[0].piece;
 	}
 
 };
