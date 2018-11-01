697201414536a7f7c8caaf9e5e8f1b5e7f149e51
kyranet <kyradiscord@gmail.com>
2018-11-01 22:39:36 +0100

    fix: Configuration commands not checking correctly

--- src/commands/Admin/conf.js
+++ src/commands/Admin/conf.js
@@ -27,7 +27,7 @@ module.exports = class extends Command {
 
 	show(message, [key]) {
 		const piece = this.getPath(key);
-		if (!piece || piece.type === 'Folder' ? !piece.configurableKeys.length : !piece.configurable) return message.sendLocale('COMMAND_CONF_GET_NOEXT', [key]);
+		if (!piece || (piece.type === 'Folder' ? !piece.configurableKeys.length : !piece.configurable)) return message.sendLocale('COMMAND_CONF_GET_NOEXT', [key]);
 		if (piece.type === 'Folder') {
 			return message.sendLocale('COMMAND_CONF_SERVER', [
 				key ? `: ${key.split('.').map(toTitleCase).join('/')}` : '',
--- src/commands/General/User Settings/userconf.js
+++ src/commands/General/User Settings/userconf.js
@@ -25,7 +25,7 @@ module.exports = class extends Command {
 
 	show(message, [key]) {
 		const piece = this.getPath(key);
-		if (!piece || piece.type === 'Folder' ? !piece.configurableKeys.length : !piece.configurable) return message.sendLocale('COMMAND_CONF_GET_NOEXT', [key]);
+		if (!piece || (piece.type === 'Folder' ? !piece.configurableKeys.length : !piece.configurable)) return message.sendLocale('COMMAND_CONF_GET_NOEXT', [key]);
 		if (piece.type === 'Folder') {
 			return message.sendLocale('COMMAND_CONF_USER', [
 				key ? `: ${key.split('.').map(toTitleCase).join('/')}` : '',
