8ad88eb416133bee66c1ed5b16a6e82c4528eca1
kyranet <kyradiscord@gmail.com>
2018-11-02 17:48:34 +0100

    fix: Init schedule before pieces (but after SG)

--- src/events/onceReady.js
+++ src/events/onceReady.js
@@ -19,13 +19,13 @@ module.exports = class extends Event {
 		this.client.settings = clientStorage.create(this.client, this.client.user.id);
 		await this.client.gateways.sync();
 
+		// Init the schedule
+		await this.client.schedule.init();
+
 		// Init all the pieces
 		await Promise.all(this.client.pieceStores.filter(store => !['providers', 'extendables'].includes(store.name)).map(store => store.init()));
 		util.initClean(this.client);
 		this.client.ready = true;
-
-		// Init the schedule
-		await this.client.schedule.init();
 
 		if (this.client.options.readyMessage !== null) {
 			this.client.emit('log', util.isFunction(this.client.options.readyMessage) ? this.client.options.readyMessage(this.client) : this.client.options.readyMessage);
