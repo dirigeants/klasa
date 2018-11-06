836b475ead9e0b2db2866f8d09287f7f9af30a4d
Kyra <kyradiscord@gmail.com>
2018-11-06 17:51:34 +0100

    feat: Added `KlasaClient#settingsSync` event. (#475)
    * feat: Added `KlasaClient#settingsSyncEntry` event.
    
    * docs: Correct PR number in CHANGELOG
    
    * src: Emit settingsSyncEntry on broadcastEval
    
    * fix: Entry settings not setting their existence status to true in shards
    
    * misc: Renamed `KlasaClient#{settingsUpdateEntry,settingsDeleteEntry,settingsCreateEntry}` to `KlasaClient#{settingsUpdate,settingsDelete,settingsCreate}`.
    
    * misc: Rename prefix settingsUpdate core event to... coreSettingsUpdate
    
    * fix: coreSettingsUpdate is not an once event

---
+++ src/events/coreSettingsUpdate.js
@@ -0,0 +1,29 @@
+const { Event } = require('klasa');
+const gateways = ['users', 'clientStorage'];
+
+module.exports = class extends Event {
+
+	constructor(...args) {
+		super(...args, { event: 'settingsUpdate' });
+	}
+
+	run(settings) {
+		if (this.client.shard && gateways.includes(settings.gateway.name)) {
+			this.client.shard.broadcastEval(`
+				if (this.shard.id !== ${this.client.shard.id}) {
+					const entry = this.gateways.get('${settings.gateway.name}').get('${settings.id}');
+					if (entry) {
+						entry._patch(${JSON.stringify(settings)});
+						entry.existenceStatus = true;
+						this.emit('settingsSync', settings);
+					}
+				}
+			`);
+		}
+	}
+
+	init() {
+		if (!this.client.shard) this.disable();
+	}
+
+};
