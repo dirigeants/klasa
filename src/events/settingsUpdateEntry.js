4183b048e6a26629c872dc7f1f3dc94105dd1829
Kyra <kyradiscord@gmail.com>
2018-11-07 01:21:30 +0100

    deps: Update Klasa for Internal Sharding (#473)
    * deps: Update ping command for Internal Sharding
    
    * fix: settingsUpdateEntry event handler for IS
    
    * fix: Update all broadcast evals and ScheduledTask.createID for IS
    
    * misc: Remove KlasaConsole#client, move shard to commandLogging
    
    * fix: Malformed broadcastEval scripts
    
    * really update the shard info in the command_stats lang

--- src/events/settingsUpdateEntry.js
+++ src/events/settingsUpdateEntry.js
@@ -4,14 +4,13 @@ const gateways = ['users', 'clientStorage'];
 module.exports = class extends Event {
 
 	run(settings) {
-		if (this.client.shard && gateways.includes(settings.gateway.type)) {
+		if (gateways.includes(settings.gateway.type)) {
 			this.client.shard.broadcastEval(`
-				if (this.shard.id !== ${this.client.shard.id}) {
-					const entry = this.gateways.${settings.gateway.type}.get('${settings.id}');
-					if (entry) {
-						entry._patch(${JSON.stringify(settings)});
-						entry._existsInDB = true;
-					}
+				if (String(this.shard.id) === '${this.client.shard.id}') return;
+				const entry = this.gateways.${settings.gateway.type}.get('${settings.id}');
+				if (entry) {
+					entry._patch(${JSON.stringify(settings)});
+					entry._existsInDB = true;
 				}
 			`);
 		}
