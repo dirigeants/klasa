124ad4a23e46a257264fefa4e940869c0f2ffae6
Faith <tba28012@hotmail.com>
2018-11-10 17:27:08 -0500

    fix: my brain

--- src/events/settingsUpdateEntry.js
+++ src/events/settingsUpdateEntry.js
@@ -6,7 +6,7 @@ module.exports = class extends Event {
 	run(settings) {
 		if (gateways.includes(settings.gateway.type)) {
 			this.client.shard.broadcastEval(`
-				if (String(this.shard.id)) !== '${this.client.shard.id}') {
+				if (String(this.shard.id) !== '${this.client.shard.id}') {
 					const entry = this.gateways.${settings.gateway.type}.get('${settings.id}');
 					if (entry) {
 						entry._patch(${JSON.stringify(settings)});
