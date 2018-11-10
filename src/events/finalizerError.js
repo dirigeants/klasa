4fdade46c9c6cb1acfb62e046a734df7373c7a45
Frangu Vlad <kingdgrizzle@gmail.com>
2018-11-10 23:59:35 +0200

    fix: finalizerError not receiving proper args (#483)

--- src/events/finalizerError.js
+++ src/events/finalizerError.js
@@ -2,7 +2,7 @@ const { Event } = require('klasa');
 
 module.exports = class extends Event {
 
-	run(message, response, timer, finalizer, error) {
+	run(message, command, response, timer, finalizer, error) {
 		this.client.emit('wtf', `[FINALIZER] ${finalizer.path}\n${error ?
 			error.stack ? error.stack : error : 'Unknown error'}`);
 	}
