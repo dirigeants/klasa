8623a7eba2060e3d0c4123b82d77daf014d55181
Faith <tba28012@hotmail.com>
2018-11-10 16:51:12 -0500

    fix: illegal return

--- src/events/settingsUpdateEntry.js
+++ src/events/settingsUpdateEntry.js
@@ -6,11 +6,12 @@ module.exports = class extends Event {
 	run(settings) {
 		if (gateways.includes(settings.gateway.type)) {
 			this.client.shard.broadcastEval(`
-				if (String(this.shard.id) === '${this.client.shard.id}') return;
-				const entry = this.gateways.${settings.gateway.type}.get('${settings.id}');
-				if (entry) {
-					entry._patch(${JSON.stringify(settings)});
-					entry._existsInDB = true;
+				if (String(this.shard.id)) !== '${this.client.shard.id}') {
+					const entry = this.gateways.${settings.gateway.type}.get('${settings.id}');
+					if (entry) {
+						entry._patch(${JSON.stringify(settings)});
+						entry._existsInDB = true;
+					}
 				}
 			`);
 		}
