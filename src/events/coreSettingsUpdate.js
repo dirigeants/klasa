49b9ba6fa8e61744896a201cc9e0a3e9597fb1ea
Faith <tba28012@hotmail.com>
2018-11-11 03:00:24 -0500

    fix: bugfixes from #485

--- src/events/coreSettingsUpdate.js
+++ src/events/coreSettingsUpdate.js
@@ -10,12 +10,13 @@ module.exports = class extends Event {
 	run(settings) {
 		if (gateways.includes(settings.gateway.name)) {
 			this.client.shard.broadcastEval(`
-				if (String(this.shard.id) === '${this.client.shard.id}') return;
-				const entry = this.gateways.get('${settings.gateway.name}').get('${settings.id}');
-				if (entry) {
-					entry._patch(${JSON.stringify(settings)});
-					entry.existenceStatus = true;
-					this.emit('settingsSync', settings);
+				if (String(this.shard.id) !== '${this.client.shard.id}') {
+					const entry = this.gateways.get('${settings.gateway.name}').get('${settings.id}');
+					if (entry) {
+						entry._patch(${JSON.stringify(settings)});
+						entry.existenceStatus = true;
+						this.emit('settingsSync', settings);
+					}
 				}
 			`);
 		}
