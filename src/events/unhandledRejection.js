dfba17e75c1203906de9be59f30f69d6389123a3
Unknown <bdistin@gmail.com>
2018-11-30 11:19:54 -0600

    the unhandledRejection event should be unloaded if production is true

--- src/events/unhandledRejection.js
+++ src/events/unhandledRejection.js
@@ -4,15 +4,12 @@ module.exports = class extends Event {
 
 	constructor(...args) {
 		super(...args, { emitter: process });
+		if (this.client.options.production) this.unload();
 	}
 
 	run(err) {
 		if (!err) return;
 		this.client.emit('error', `Uncaught Promise Error: \n${err.stack || err}`);
-	}
-
-	init() {
-		if (this.client.options.production) this.disable();
 	}
 
 };
