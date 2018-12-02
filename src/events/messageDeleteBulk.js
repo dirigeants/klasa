b2c422b7697d28db695e22c91b4c2d2e11202afe
Kyra <kyradiscord@gmail.com>
2018-12-02 16:17:29 +0100

    fix: messageDeleteBulk not sweeping

--- src/events/messageDeleteBulk.js
+++ src/events/messageDeleteBulk.js
@@ -3,9 +3,9 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(messages) {
-		for (const message of messages) {
+		for (const message of messages.values()) {
 			if (message.command && message.command.deletable) {
-				for (const msg of message.responses) {
+				for (const msg of message.responses.values()) {
 					if (!msg.deleted) msg.delete();
 				}
 			}
