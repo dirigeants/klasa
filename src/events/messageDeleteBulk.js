e7df83ccda95c301992625a3beac3fd05718e9bf
Kyra <kyradiscord@gmail.com>
2018-12-02 16:19:54 +0100

    fix: KlasaMessage#responses is an array
    And the getter filters deleted.

--- src/events/messageDeleteBulk.js
+++ src/events/messageDeleteBulk.js
@@ -5,8 +5,8 @@ module.exports = class extends Event {
 	run(messages) {
 		for (const message of messages.values()) {
 			if (message.command && message.command.deletable) {
-				for (const msg of message.responses.values()) {
-					if (!msg.deleted) msg.delete();
+				for (const msg of message.responses) {
+					msg.delete();
 				}
 			}
 		}
