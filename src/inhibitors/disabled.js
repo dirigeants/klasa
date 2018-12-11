ad8f6a6ce6f55131f25764562c39f5cdcf854604
Faith <tba28012@hotmail.com>
2018-12-11 18:04:18 -0500

    src: make piece serializer better and add aliases (#541)
    * src: make piece serializer better and add aliases
    
    * src: fix build
    
    * Update src/languages/en-US.js
    
    Co-Authored-By: UnseenFaith <tba28012@hotmail.com>

--- src/inhibitors/disabled.js
+++ src/inhibitors/disabled.js
@@ -4,7 +4,7 @@ module.exports = class extends Inhibitor {
 
 	run(message, command) {
 		if (!command.enabled) throw message.language.get('INHIBITOR_DISABLED_GLOBAL');
-		if (message.guildSettings.disabledCommands.includes(command.name)) throw message.language.get('INHIBITOR_DISABLED_GUILD');
+		if (message.guildSettings.get('disabledCommands').includes(command.name)) throw message.language.get('INHIBITOR_DISABLED_GUILD');
 	}
 
 };
--- src/serializers/piece.js
+++ src/serializers/piece.js
@@ -3,11 +3,22 @@ const { Serializer } = require('klasa');
 module.exports = class extends Serializer {
 
 	constructor(...args) {
-		super(...args, { aliases: ['command', 'language'] });
+		super(...args);
+
+		// Adds all pieces, custom or not, to this serialize piece for use in schemas
+		this.aliases = [...this.client.pieceStores.keys()].map(type => type.slice(0, -1));
 	}
 
 	async deserialize(data, piece, language) {
-		const store = this.client[`${piece.type}s`];
+		if (piece.type === 'piece') {
+			for (const store of this.client.pieceStores.values()) {
+				const pce = store.get(data);
+				if (pce) return pce;
+			}
+			throw language.get('RESOLVER_INVALID_PIECE', piece.key, piece.type);
+		}
+		const store = this.client.pieceStores.get(`${piece.type}s`);
+		if (!store) throw language.get('RESOLVER_INVALID_STORE', piece.type);
 		const parsed = typeof data === 'string' ? store.get(data) : data;
 		if (parsed && parsed instanceof store.holds) return parsed;
 		throw language.get('RESOLVER_INVALID_PIECE', piece.key, piece.type);
