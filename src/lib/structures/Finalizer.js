1d062ab5a49828cf7d474c31bf2ae9cba81618ff
bdistin <bdistin@gmail.com>
2018-12-11 10:23:57 -0600

    Correct some OOP placement of private methods (#540)
    * correct some oop placement of private methods
    
    * typings: Update for latest commit
    
    * docs: Fixed typos and descriptions

--- src/lib/structures/Finalizer.js
+++ src/lib/structures/Finalizer.js
@@ -9,11 +9,28 @@ const Piece = require('./base/Piece');
 class Finalizer extends Piece {
 
 	/**
+	 * Run a finalizer and catch any uncaught promises
+	 * @since 0.5.0
+	 * @param {KlasaMessage} message The message that called the command
+	 * @param {Command} command The command this finalizer is for (may be different than message.command)
+	 * @param {?KlasaMessage|KlasaMessage[]} response The bot's response message, if one is returned
+	 * @param {Stopwatch} runTime The time it took to generate the command
+	 * @private
+	 */
+	async _run(message, command, response, runTime) {
+		try {
+			await this.run(message, command, response, runTime);
+		} catch (err) {
+			this.client.emit('finalizerError', message, command, response, runTime, this, err);
+		}
+	}
+
+	/**
 	 * The run method to be overwritten in actual finalizers
 	 * @since 0.0.1
 	 * @param {KlasaMessage} message The message used to trigger this finalizer
 	 * @param {Command} command The command this finalizer is for (may be different than message.command)
-	 * @param {KlasaMessage|KlasaMessage[]} response The bot's response message, if one is returned
+	 * @param {?KlasaMessage|KlasaMessage[]} response The bot's response message, if one is returned
 	 * @param {Stopwatch} runTime The time it took to generate the command
 	 * @returns {void}
 	 * @abstract
