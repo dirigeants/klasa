c2dfd9571b04840a0f53b5320d095683459d44ba
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-01-18 12:06:52 -0500

    Update info.js

--- src/commands/General/Chat Bot Info/info.js
+++ src/commands/General/Chat Bot Info/info.js
@@ -6,7 +6,7 @@ module.exports = class extends Command {
 		super(...args, {
 			aliases: ['details', 'what'],
 			guarded: true,
-			description: language => language.get('COMMAND_INFO_DESCRIPTION')
+			description: ({ language }) => language.get('COMMAND_INFO_DESCRIPTION')
 		});
 	}
 
