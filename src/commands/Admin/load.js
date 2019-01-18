2fd8fb0a25d88d5f50b0802df314f9989317f0b1
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-01-18 12:04:37 -0500

    Update load.js

--- src/commands/Admin/load.js
+++ src/commands/Admin/load.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
 			aliases: ['l'],
 			permissionLevel: 10,
 			guarded: true,
-			description: language => language.get('COMMAND_LOAD_DESCRIPTION'),
+			description: ({ language }) => language.get('COMMAND_LOAD_DESCRIPTION'),
 			usage: '[core] <Store:store> <path:...string>',
 			usageDelim: ' '
 		});
