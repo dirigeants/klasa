f597d8d407f90a6797812fb11f70b4576be1cce3
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-01-18 12:07:14 -0500

    Update stats.js

--- src/commands/General/Chat Bot Info/stats.js
+++ src/commands/General/Chat Bot Info/stats.js
@@ -6,7 +6,7 @@ module.exports = class extends Command {
 	constructor(...args) {
 		super(...args, {
 			guarded: true,
-			description: language => language.get('COMMAND_STATS_DESCRIPTION')
+			description: ({ language }) => language.get('COMMAND_STATS_DESCRIPTION')
 		});
 	}
 
