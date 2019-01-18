dc2fabf0d86d7b04e3a0a17176a779894e6df8be
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-01-18 12:05:01 -0500

    Update transfer.js

--- src/commands/Admin/transfer.js
+++ src/commands/Admin/transfer.js
@@ -8,7 +8,7 @@ module.exports = class extends Command {
 		super(...args, {
 			permissionLevel: 10,
 			guarded: true,
-			description: language => language.get('COMMAND_TRANSFER_DESCRIPTION'),
+			description: ({ language }) => language.get('COMMAND_TRANSFER_DESCRIPTION'),
 			usage: '<Piece:piece>'
 		});
 	}
