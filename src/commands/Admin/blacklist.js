1c87894ee6e89806b0b8d1ec5f08462e22fb9a4e
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-01-18 11:59:48 -0500

    Update blacklist.js

--- src/commands/Admin/blacklist.js
+++ src/commands/Admin/blacklist.js
@@ -6,7 +6,7 @@ module.exports = class extends Command {
 	constructor(...args) {
 		super(...args, {
 			permissionLevel: 10,
-			description: language => language.get('COMMAND_BLACKLIST_DESCRIPTION'),
+			description: ({ language }) => language.get('COMMAND_BLACKLIST_DESCRIPTION'),
 			usage: '<User:user|Guild:guild|guild:str> [...]',
 			usageDelim: ' ',
 			guarded: true
