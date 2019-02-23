1401e42ef45514260fe81d4ec0df6a757b3dd613
Unknown <bdistin@gmail.com>
2019-02-23 12:53:47 -0600

    add missing .catch(()=>null)s

--- src/arguments/channel.js
+++ src/arguments/channel.js
@@ -4,10 +4,10 @@ module.exports = class extends Argument {
 
 	async run(arg, possible, message) {
 		// DM Channel support
-		const user = this.constructor.regex.userOrMember.test(arg) ? await this.client.users.fetch(this.constructor.regex.userOrMember.exec(arg)[1]) : null;
+		const user = this.constructor.regex.userOrMember.test(arg) ? await this.client.users.fetch(this.constructor.regex.userOrMember.exec(arg)[1]).catch(() => null) : null;
 		if (user) return user.createDM();
 		// Regular Channel support
-		const channel = this.constructor.regex.channel.test(arg) ? await this.client.channels.fetch(this.constructor.regex.channel.exec(arg)[1]) : null;
+		const channel = this.constructor.regex.channel.test(arg) ? await this.client.channels.fetch(this.constructor.regex.channel.exec(arg)[1]).catch(() => null) : null;
 		if (channel) return channel;
 		throw message.language.get('RESOLVER_INVALID_CHANNEL', possible.name);
 	}
