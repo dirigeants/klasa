0c95fab1a16ccd400ce44bcb64451abcd296c5c3
Unknown <bdistin@gmail.com>
2019-02-23 12:39:08 -0600

    fix some partials related things with Arguments

--- src/arguments/channel.js
+++ src/arguments/channel.js
@@ -2,8 +2,12 @@ const { Argument } = require('klasa');
 
 module.exports = class extends Argument {
 
-	run(arg, possible, message) {
-		const channel = this.constructor.regex.channel.test(arg) ? this.client.channels.get(this.constructor.regex.channel.exec(arg)[1]) : null;
+	async run(arg, possible, message) {
+		// DM Channel support
+		const user = this.constructor.regex.userOrMember.test(arg) ? await this.client.users.fetch(this.constructor.regex.userOrMember.exec(arg)[1]) : null;
+		if (user) return user.createDM();
+		// Regular Channel support
+		const channel = this.constructor.regex.channel.test(arg) ? await this.client.channels.fetch(this.constructor.regex.channel.exec(arg)[1]) : null;
 		if (channel) return channel;
 		throw message.language.get('RESOLVER_INVALID_CHANNEL', possible.name);
 	}
--- src/arguments/member.js
+++ src/arguments/member.js
@@ -3,15 +3,8 @@ const { Argument } = require('klasa');
 module.exports = class extends Argument {
 
 	async run(arg, possible, message) {
-		let member = message.guild.members.resolve(arg);
+		const member = this.constructor.regex.userOrMember.test(arg) ? await message.guild.members.fetch(this.constructor.regex.userOrMember.exec(arg)[1]).catch(() => null) : null;
 		if (member) return member;
-		if (this.constructor.regex.userOrMember.test(arg)) {
-			const user = await this.client.users.fetch(this.constructor.regex.userOrMember.exec(arg)[1]).catch(() => null);
-			if (user) {
-				member = await message.guild.members.fetch(user).catch(() => null);
-				if (member) return member;
-			}
-		}
 		throw message.language.get('RESOLVER_INVALID_MEMBER', possible.name);
 	}
 
