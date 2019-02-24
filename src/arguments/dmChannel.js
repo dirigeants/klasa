bf98495c0c6250fb2817656a455f60065098e833
Unknown <bdistin@gmail.com>
2019-02-24 08:51:37 -0600

    make the 3 scoped channel arguments independent of the main channel argument

--- src/arguments/dmChannel.js
+++ src/arguments/dmChannel.js
@@ -3,8 +3,8 @@ const { Argument } = require('klasa');
 module.exports = class extends Argument {
 
 	async run(arg, possible, message) {
-		const channel = await this.store.get('channel').run(arg, possible, message);
-		if (channel && channel.type === 'dm') return channel;
+		const user = this.constructor.regex.userOrMember.test(arg) ? await this.client.users.fetch(this.constructor.regex.userOrMember.exec(arg)[1]).catch(() => null) : null;
+		if (user) return user.createDM();
 		throw message.language.get('RESOLVER_INVALID_CHANNEL', possible.name);
 	}
 
--- src/arguments/voiceChannel.js
+++ src/arguments/voiceChannel.js
@@ -3,7 +3,7 @@ const { Argument } = require('klasa');
 module.exports = class extends Argument {
 
 	async run(arg, possible, message) {
-		const channel = await this.store.get('channel').run(arg, possible, message);
+		const channel = this.constructor.regex.channel.test(arg) ? await this.client.channels.fetch(this.constructor.regex.channel.exec(arg)[1]).catch(() => null) : null;
 		if (channel && channel.type === 'voice') return channel;
 		throw message.language.get('RESOLVER_INVALID_CHANNEL', possible.name);
 	}
