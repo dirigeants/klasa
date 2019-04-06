7e635247297ee7e14fabe10e9fce285788a023d6
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-04-06 12:41:56 -0400

    Update eventError.js

--- src/events/eventError.js
+++ src/events/eventError.js
@@ -4,7 +4,7 @@ module.exports = class extends Event {
 
 	run(event, args, error) {
 		const errorMessage = `[EVENT] ${event.path}\n${error ?
-			error.stack ? error.stack : error : 'Unknown error'}`
+			error.stack ? error.stack : error : 'Unknown error'}`;
 		this.client.emit('wtf', errorMessage);
 		this.client.emit('discordLog', errorMessage);
 	}
