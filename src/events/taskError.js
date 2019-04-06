c654e68652d6a0ea11d88e80e7f4a5c28437e038
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-04-06 12:30:05 -0400

    Update taskError.js

--- src/events/taskError.js
+++ src/events/taskError.js
@@ -3,8 +3,10 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(scheduledTask, task, error) {
-		this.client.emit('wtf', `[TASK] ${task.path}\n${error ?
-			error.stack ? error.stack : error : 'Unknown error'}`);
+		const errorMessage = `[TASK] ${task.path}\n${error ?
+			error.stack ? error.stack : error : 'Unknown error'}`;
+		this.client.emit('wtf', errorMessage);
+		this.client.emit('discordLog', errorMessage);
 	}
 
 };
