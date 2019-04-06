da44d37f68e1f7ef296d9b03092ac8c84b2a56ad
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-04-06 12:27:40 -0400

    Update commandError.js

--- src/events/commandError.js
+++ src/events/commandError.js
@@ -3,7 +3,11 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(message, command, params, error) {
-		if (error instanceof Error) this.client.emit('wtf', `[COMMAND] ${command.path}\n${error.stack || error}`);
+		if (error instanceof Error) {
+			const errorMessage = `[COMMAND] ${command.path}\n${error.stack || error}`;
+			this.client.emit('wtf', errorMessage);
+			this.client.emit('discordLog', errorMessage);
+		}
 		if (error.message) message.sendCode('JSON', error.message).catch(err => this.client.emit('wtf', err));
 		else message.sendMessage(error).catch(err => this.client.emit('wtf', err));
 	}
