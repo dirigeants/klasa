b70c578299628331d7026c10f14960c5c71dd523
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-04-06 12:29:04 -0400

    Update finalizerError.js

--- src/events/finalizerError.js
+++ src/events/finalizerError.js
@@ -3,8 +3,10 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	run(message, command, response, timer, finalizer, error) {
-		this.client.emit('wtf', `[FINALIZER] ${finalizer.path}\n${error ?
-			error.stack ? error.stack : error : 'Unknown error'}`);
+		const errorMessage = `[FINALIZER] ${finalizer.path}\n${error ?
+			error.stack ? error.stack : error : 'Unknown error'}`;
+		this.client.emit('wtf', errorMessage);
+		this.client.emit('discordLog', errorMessage);
 	}
 
 };
