ef68921aa5b3ab6d45a9f0e11ed12215eace267c
bdistin <bdistin@gmail.com>
2019-05-15 11:00:54 -0500

    rename event files that have core klasa functionality

--- src/events/coreMessage.js
+++ src/events/coreMessage.js
@@ -2,6 +2,10 @@ const { Event } = require('klasa');
 
 module.exports = class extends Event {
 
+	constructor(...args) {
+		super(...args, { event: 'message' });
+	}
+
 	run(message) {
 		if (this.client.ready) this.client.monitors.run(message);
 	}
