66a3a51142e0289095a84e148f9b1213ae878e79
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-07-19 16:06:54 -0400

    Update cooldown.js

--- src/inhibitors/cooldown.js
+++ src/inhibitors/cooldown.js
@@ -13,7 +13,7 @@ module.exports = class extends Inhibitor {
 
 		if (existing && existing.limited) throw message.language.get('INHIBITOR_COOLDOWN', this.secondsToFormattedString(Math.ceil(existing.remainingTime / 1000)), command.cooldownLevel !== 'author');
 	}
-	
+
 	secondsToFormattedString(time) {
 		const days = Math.floor(time / 86400);
 		const hours = Math.floor(time / 3600);
