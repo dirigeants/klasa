71e67afdabb0e6b1097cafcc5a29ee362b1b14e2
Vlad Frangu <kingdgrizzle@gmail.com>
2019-07-20 01:06:24 +0300

    src: Add support for the new channel mention format

--- src/lib/util/constants.js
+++ src/lib/util/constants.js
@@ -240,7 +240,7 @@ exports.TIME.CRON.tokensRegex = new RegExp(Object.keys(exports.TIME.CRON.tokens)
 
 exports.MENTION_REGEX = {
 	userOrMember: /^(?:<@!?)?(\d{17,19})>?$/,
-	channel: /^(?:<#)?(\d{17,19})>?$/,
+	channel: /^(?:<#)?(\d{17,19})(?::\d{17,19}:[^>]+)?>?$/,
 	emoji: /^(?:<a?:\w{2,32}:)?(\d{17,19})>?$/,
 	role: /^(?:<@&)?(\d{17,19})>?$/,
 	snowflake: /^(\d{17,19})$/
