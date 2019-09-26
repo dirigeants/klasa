ee66086b85963be8e96f7d0db92d40485eb34cb9
UnseenFaith <tba28012@hotmail.com>
2019-09-26 13:05:49 -0400

    delete should also emit sync to send defaults back

--- src/events/coreSettingsDelete.js
+++ src/events/coreSettingsDelete.js
@@ -7,15 +7,15 @@ module.exports = class extends Event {
 		super(...args, { event: 'settingsDelete' });
 	}
 
-	run(settings, broadcast = true) {
-		if (broadcast && gateways.includes(settings.gateway.name)) {
+	run(settings) {
+		if (gateways.includes(settings.gateway.name)) {
 			this.client.shard.broadcastEval(`
 				if (String(this.options.shards) !== '${this.client.options.shards}') {
 					const entry = this.gateways.get('${settings.gateway.name}').get('${settings.id}');
 					if (entry && entry.existenceStatus) {
-						this.emit('settingsDelete', entry, false);
 						entry.init(entry, entry.schema);
 						entry.existenceStatus = false;
+						this.emit('settingsSync', entry);
 					}
 				}
 			`);
