a11afe4e8160e5adcfe7779280c728c3589f3b44
Gryffon Bellish <39341355+PyroTechniac@users.noreply.github.com>
2019-09-30 11:33:49 -0400

    Update coreSettingsUpdate.js

--- src/events/coreSettingsUpdate.js
+++ src/events/coreSettingsUpdate.js
@@ -5,10 +5,11 @@ module.exports = class extends Event {
 
 	constructor(...args) {
 		super(...args, { event: 'settingsUpdate' });
+		this.enabled = Boolean(this.client.shard)
 	}
 
 	run(settings, updateObject) {
-		if (gateways.includes(settings.gateway.name) && this.client.ready) {
+		if (gateways.includes(settings.gateway.name)) {
 			this.client.shard.broadcastEval(`
 				if (String(this.options.shards) !== '${this.client.options.shards}') {
 					const entry = this.gateways.get('${settings.gateway.name}').get('${settings.id}');
