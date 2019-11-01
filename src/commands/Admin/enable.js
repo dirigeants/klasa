b4b6dd2d448db47e91f4948f3834baac3ba85496
Gryffon Bellish <owenbellish@gmail.com>
2019-11-01 13:48:32 -0400

    Update enable.js

--- src/commands/Admin/enable.js
+++ src/commands/Admin/enable.js
@@ -18,7 +18,7 @@ module.exports = class extends Command {
 				if (String(this.options.shards) !== '${this.client.options.shards}') this.${piece.store}.get('${piece.name}').enable();
 			`);
 		}
-		return message.sendCode('diff', message.language.get('COMMAND_ENABLE', piece.type, piece.name));
+		return message.sendLocale('COMMAND_ENABLE', [piece.type, piece.name], { code: 'diff' });
 	}
 
 };
