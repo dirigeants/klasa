a9aac33e9e12f4a8abb06c1c61bb86917858c2b5
Jeroen Claassens <jeroen.claassens@live.nl>
2020-05-07 23:53:56 +0200

    fix(typings): add `news` as option for `runIn` (#982)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1298,7 +1298,7 @@ declare module 'klasa' {
 		promptTime?: number;
 		quotedStringSupport?: boolean;
 		requiredSettings?: string[];
-		runIn?: Array<'text' | 'dm'>;
+		runIn?: Array<'text' | 'dm' | 'news'>;
 		subcommands?: boolean;
 		usage?: string;
 		usageDelim?: string;
