c1d75a18f713870ec4adea3a251ac7ee6af7be05
bdistin <bdistin@gmail.com>
2020-05-26 20:28:44 -0500

    fix argument error

--- src/events/argumentError.ts
+++ src/events/argumentError.ts
@@ -1,12 +1,10 @@
 import { Event } from '@klasa/core';
-import { codeblock } from 'discord-md-tags';
 import type { Argument, KlasaMessage } from 'klasa';
 
 export default class extends Event {
 
-	public run(message: KlasaMessage, argument: Argument, _params: readonly unknown[], error: Error): void {
-		this.client.emit('wtf', `[ARGUMENT] ${argument.path}\n${error.stack || error}`);
-		message.send(mb => mb.setContent(codeblock('JSON') `${error.message}`)).catch(err => this.client.emit('wtf', err));
+	public run(message: KlasaMessage, _argument: Argument, _params: readonly unknown[], error: string): void {
+		message.send(mb => mb.setContent(error)).catch(err => this.client.emit('wtf', err));
 	}
 
 }
