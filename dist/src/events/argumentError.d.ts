5d1fdeb630db537967f2ba0a4ceba78bc698262a
bdistin <bdistin@users.noreply.github.com>
2020-05-27 01:29:22 +0000

    TSC build: c1d75a18f713870ec4adea3a251ac7ee6af7be05

--- dist/src/events/argumentError.d.ts
+++ dist/src/events/argumentError.d.ts
@@ -1,5 +1,5 @@
 import { Event } from '@klasa/core';
 import type { Argument, KlasaMessage } from 'klasa';
 export default class extends Event {
-    run(message: KlasaMessage, argument: Argument, _params: readonly unknown[], error: Error): void;
+    run(message: KlasaMessage, _argument: Argument, _params: readonly unknown[], error: string): void;
 }
--- dist/src/events/argumentError.js
+++ dist/src/events/argumentError.js
@@ -1,11 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@klasa/core");
-const discord_md_tags_1 = require("discord-md-tags");
 class default_1 extends core_1.Event {
-    run(message, argument, _params, error) {
-        this.client.emit('wtf', `[ARGUMENT] ${argument.path}\n${error.stack || error}`);
-        message.send(mb => mb.setContent(discord_md_tags_1.codeblock('JSON') `${error.message}`)).catch(err => this.client.emit('wtf', err));
+    run(message, _argument, _params, error) {
+        message.send(mb => mb.setContent(error)).catch(err => this.client.emit('wtf', err));
     }
 }
 exports.default = default_1;
