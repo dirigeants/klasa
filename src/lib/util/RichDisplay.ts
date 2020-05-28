b41ed5b48c6c7c91f356f6d59816c6f1bebcbd19
Gryffon Bellish <owenbellish@gmail.com>
2020-05-28 18:18:16 -0400

    linting, this is all I could do

--- src/lib/util/RichDisplay.ts
+++ src/lib/util/RichDisplay.ts
@@ -1,93 +1,95 @@
-import { Embed, EmojiResolvable } from "@klasa/core";
+import { Embed, EmojiResolvable } from '@klasa/core';
 
 export interface RichDisplayEmojisObject {
-    first: EmojiResolvable;
-    back: EmojiResolvable;
-    forward: EmojiResolvable;
-    last: EmojiResolvable;
-    jump: EmojiResolvable;
-    info: EmojiResolvable;
-    stop: EmojiResolvable;
+	first: EmojiResolvable;
+	back: EmojiResolvable;
+	forward: EmojiResolvable;
+	last: EmojiResolvable;
+	jump: EmojiResolvable;
+	info: EmojiResolvable;
+	stop: EmojiResolvable;
 }
 
 export type EmbedResolvable = Embed | ((embed: Embed) => Embed);
 
 export class RichDisplay {
-    public embedTemplate: Embed;
-    public pages: Embed[];
-    public infoPage: Embed | null;
-    public emojis: RichDisplayEmojisObject;
-    public footered: boolean;
-    public footerPrefix: string;
-    public footerSuffix: string;
-    
-    public constructor(embed: Embed = new Embed()) {
-        this.embedTemplate = embed;
-        this.pages = [];
-        this.infoPage = null;
-        this.emojis = {
-            first: '⏮',
+
+	public embedTemplate: Embed;
+	public pages: Embed[];
+	public infoPage: Embed | null;
+	public emojis: RichDisplayEmojisObject;
+	public footered: boolean;
+	public footerPrefix: string;
+	public footerSuffix: string;
+
+	public constructor(embed: Embed = new Embed()) {
+		this.embedTemplate = embed;
+		this.pages = [];
+		this.infoPage = null;
+		this.emojis = {
+			first: '⏮',
 			back: '◀',
 			forward: '▶',
 			last: '⏭',
 			jump: '🔢',
 			info: 'ℹ',
 			stop: '⏹'
-        };
-        this.footered = false;
-        this.footerPrefix = '';
-        this.footerSuffix = '';
-    }
+		};
+		this.footered = false;
+		this.footerPrefix = '';
+		this.footerSuffix = '';
+	}
 
-    public get template(): Embed {
-        return new Embed(this.embedTemplate);
-    }
+	public get template(): Embed {
+		return new Embed(this.embedTemplate);
+	}
 
-    public setEmojis(emojis: Partial<RichDisplayEmojisObject>): this {
-        Object.assign(this.emojis, emojis);
-        return this;
-    }
+	public setEmojis(emojis: Partial<RichDisplayEmojisObject>): this {
+		Object.assign(this.emojis, emojis);
+		return this;
+	}
 
-    public setFooterPrefix(prefix: string): this {
-        this.footered = false;
-        this.footerPrefix = prefix;
-        return this;
-    }
+	public setFooterPrefix(prefix: string): this {
+		this.footered = false;
+		this.footerPrefix = prefix;
+		return this;
+	}
 
-    public setFooterSuffix(suffix: string): this {
-        this.footered = false;
-        this.footerSuffix = suffix;
-        return this;
-    }
+	public setFooterSuffix(suffix: string): this {
+		this.footered = false;
+		this.footerSuffix = suffix;
+		return this;
+	}
 
-    public useCustomFooters(): this {
-        this.footered = true;
-        return this;
-    }
+	public useCustomFooters(): this {
+		this.footered = true;
+		return this;
+	}
 
-    public addPage(page: Embed | ((embed: Embed) => Embed)): this {
-        this.pages.push(this._handlePageGeneration(page));
-        return this;
-    }
+	public addPage(page: Embed | ((embed: Embed) => Embed)): this {
+		this.pages.push(this._handlePageGeneration(page));
+		return this;
+	}
 
-    public setInfoPage(embed: EmbedResolvable): this {
-        this.infoPage = this._handlePageGeneration(embed);
-        return this;
-    }
+	public setInfoPage(embed: EmbedResolvable): this {
+		this.infoPage = this._handlePageGeneration(embed);
+		return this;
+	}
 
-    private _footer(): void {
-        for (let i = 1; i <= this.pages.length; i++) this.pages[i - 1].setFooter(`${this.footerPrefix}${i}/${this.pages.length}${this.footerSuffix}`);
-        if (this.infoPage) this.infoPage.setFooter('ℹ');
-    }
+	private _footer(): void {
+		for (let i = 1; i <= this.pages.length; i++) this.pages[i - 1].setFooter(`${this.footerPrefix}${i}/${this.pages.length}${this.footerSuffix}`);
+		if (this.infoPage) this.infoPage.setFooter('ℹ');
+	}
 
-    private _handlePageGeneration(cb:EmbedResolvable): Embed {
-        if (typeof cb === 'function') {
-            const page = cb(this.template);
-            if (page instanceof Embed) return page;
-        } else if (cb instanceof Embed) {
-            return cb;
-        }
+	private _handlePageGeneration(cb:EmbedResolvable): Embed {
+		if (typeof cb === 'function') {
+			const page = cb(this.template);
+			if (page instanceof Embed) return page;
+		} else if (cb instanceof Embed) {
+			return cb;
+		}
 
-        throw new Error('Expected an Embed or Function returning an Embed.');
-    }
+		throw new Error('Expected an Embed or Function returning an Embed.');
+	}
+
 }
