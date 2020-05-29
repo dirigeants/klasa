5def77d92822579c3cc7a8865687c129335b9f8e
bdistin <bdistin@users.noreply.github.com>
2020-05-29 16:45:46 +0000

    TSC build: 614ae3fc367f2c9ee23a7748c7c8398804f26527

--- dist/src/lib/util/RichDisplay.js
+++ dist/src/lib/util/RichDisplay.js
@@ -12,7 +12,7 @@ class RichDisplay {
      * @param options The RichDisplay Options
      */
     constructor(options = {}) {
-        var _a;
+        var _a, _b, _c, _d;
         /**
          * The stored pages of the display
          * @since 0.4.0
@@ -53,13 +53,13 @@ class RichDisplay {
             .set("info" /* Info */, 'ℹ')
             .set("stop" /* Stop */, '⏹');
         // To maintain emoji order, we will delete from rather than insert according to options
-        if (!options.firstLast) {
+        if ((_b = options.firstLast) !== null && _b !== void 0 ? _b : true) {
             this._emojis.delete("first" /* First */);
             this._emojis.delete("last" /* Last */);
         }
-        if (!options.jump)
+        if ((_c = options.jump) !== null && _c !== void 0 ? _c : true)
             this._emojis.delete("jump" /* Jump */);
-        if (!options.stop)
+        if ((_d = options.stop) !== null && _d !== void 0 ? _d : true)
             this._emojis.delete("stop" /* Stop */);
     }
     /**
