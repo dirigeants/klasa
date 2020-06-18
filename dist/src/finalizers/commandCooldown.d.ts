83f2c684b094d56ab453881dd415578ecdaa435b
bdistin <bdistin@users.noreply.github.com>
2020-06-18 04:29:43 +0000

    TSC build: a5bce8e44e212d02415f40e740b63cf186bea1d2

--- dist/src/finalizers/commandCooldown.d.ts
+++
@@ -1,8 +0,0 @@
-import { Finalizer, Command } from 'klasa';
-import { RateLimit } from '@klasa/ratelimits';
-import type { Message } from '@klasa/core';
-export default class extends Finalizer {
-    cooldowns: WeakMap<object, any>;
-    run(message: Message, command: Command): void;
-    getCooldown(message: Message, command: Command): RateLimit;
-}
--- dist/src/inhibitors/cooldown.d.ts
+++
@@ -1,6 +0,0 @@
-import { Inhibitor, Command, InhibitorStore } from 'klasa';
-import type { Message } from '@klasa/core';
-export default class extends Inhibitor {
-    constructor(store: InhibitorStore, directory: string, files: readonly string[]);
-    run(message: Message, command: Command): void;
-}
