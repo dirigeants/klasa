bc46950583f693f1cf7fb751fa53bcc3a8658133
bdistin <bdistin@gmail.com>
2020-06-19 15:32:32 -0500

    fix ArgumentErrors handling non-string errors

--- src/events/argumentError.ts
+++ src/events/argumentError.ts
@@ -1,11 +1,14 @@
 import { Event, Message } from '@klasa/core';
+import { codeblock } from 'discord-md-tags';
 
 import type { Argument } from 'klasa';
 
 export default class extends Event {
 
-	public async run(message: Message, _argument: Argument, _params: readonly unknown[], error: string): Promise<void> {
-		await message.reply(mb => mb.setContent(error));
+	public async run(message: Message, argument: Argument, _params: readonly unknown[], error: Error | string): Promise<void> {
+		if (error instanceof Error) this.client.emit('wtf', `[ARGUMENT] ${argument.path}\n${error.stack || error}`);
+		if (typeof error === 'string') await message.reply(mb => mb.setContent(error));
+		else await message.reply(mb => mb.setContent(codeblock('JSON') `${error.message}`));
 	}
 
 }
