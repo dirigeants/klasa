0f6e10b0e4af14af96d2392e52e16709071b99b6
bdistin <bdistin@gmail.com>
2020-06-24 15:06:55 -0500

    update missingBotPermissions

--- src/inhibitors/missingBotPermissions.ts
+++ src/inhibitors/missingBotPermissions.ts
@@ -1,7 +1,6 @@
 import { Inhibitor, Command } from 'klasa';
-import { Permissions, PermissionsFlags, Message } from '@klasa/core';
+import { Permissions, PermissionsFlags, Message, isGuildTextBasedChannel } from '@klasa/core';
 import { toTitleCase } from '@klasa/utils';
-import { ChannelType } from '@klasa/dapi-types';
 
 export default class extends Inhibitor {
 
@@ -9,16 +8,27 @@ export default class extends Inhibitor {
 	// VIEW_CHANNEL, SEND_MESSAGES, SEND_TTS_MESSAGES, EMBED_LINKS, ATTACH_FILES,
 	// READ_MESSAGE_HISTORY, MENTION_EVERYONE, USE_EXTERNAL_EMOJIS, ADD_REACTIONS
 
+	// These are permissions that can be set in a channel permission overwrite but is meaningless in the context
+	// So we add the ones we had back after calculating the permissions in the channel (after overwrites)
+	private readonly guildScopePermissions = new Permissions(1275592878).freeze();
+	// KICK_MEMBERS, BAN_MEMBERS, ADMINISTRATOR, MANAGE_GUILD, VIEW_AUDIT_LOG,
+	// VIEW_GUILD_INSIGHTS, CHANGE_NICKNAME, MANAGE_NICKNAMES, MANAGE_EMOJIS
+
 	private readonly friendlyPerms = Object.keys(Permissions.FLAGS).reduce((obj, key) => {
 		Reflect.set(obj, key, toTitleCase(key.split('_').join(' ')));
 		return obj;
 	}, {}) as Record<PermissionsFlags, string>;
 
 	public run(message: Message, command: Command): void {
-		const missing: PermissionsFlags[] = message.channel.type === ChannelType.GuildNews || message.channel.type === ChannelType.GuildText ?
+		let missing: PermissionsFlags[];
+		if (isGuildTextBasedChannel(message.channel)) {
 			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-			(message.guild!.me?.permissionsIn(message.channel).missing(command.requiredPermissions) ?? []) as PermissionsFlags[] :
-			this.impliedPermissions.missing(command.requiredPermissions) as PermissionsFlags[];
+			const guildPerms = message.guild!.me!.permissions!.mask(this.guildScopePermissions);
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			missing = (message.guild!.me!.permissionsIn(message.channel).add(guildPerms).missing(command.requiredPermissions) ?? []) as PermissionsFlags[];
+		} else {
+			missing = this.impliedPermissions.missing(command.requiredPermissions) as PermissionsFlags[];
+		}
 
 		if (missing.length) throw message.language.get('INHIBITOR_MISSING_BOT_PERMS', missing.map(key => this.friendlyPerms[key]).join(', '));
 	}
